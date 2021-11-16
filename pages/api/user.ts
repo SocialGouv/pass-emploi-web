343e11c2fcf62eac53cd1e5aadfc489506cda39d
sandra.hage.chehade <sandra.hage.chehade@octo.com>
2021-11-16 16:53:10 +0100

    feat: linter rules

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,17 +1,17 @@
-import withSession from "utils/session";
+import withSession from 'utils/session'
 
 export default withSession(async (req, res) => {
-  const user = req.session.get("user");
+  const user = req.session.get('user')
 
   if (user) {
     // check if user is already loggedIn before sending to DB
     res.json({
       isLoggedIn: true,
       ...user,
-    });
+    })
   } else {
     res.json({
       isLoggedIn: false,
-    });
+    })
   }
-});
+})
