6f71f517d0f9e05806d349b4e7e35dab8957e164
sandra.hage.chehade <sandra.hage.chehade@octo.com>
2021-11-17 11:30:26 +0100

    suppression de google tag analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,11 @@ import ProgressBar from '@badrap/bar-of-progress'
 
 import Router, { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
 
 import Layout from 'components/layouts/Layout'
 
-import * as gtag from 'utils/analytics'
-
 import 'styles/globals.css'
 import 'styles/typography.css'
-import { useEffect } from 'react'
 
 const progress = new ProgressBar({
 	size: 5,
@@ -27,43 +23,8 @@ function MyApp({ Component, pageProps }: AppProps) {
 	const router = useRouter()
 	const isLoginPage = router.pathname === '/login'
 
-	useEffect(() => {
-		const handleRouteChange = (url: URL) => {
-			gtag.pageview(url)
-		}
-		//When the component is mounted, subscribe to router changes
-		//and log those page views
-		router.events.on('routeChangeComplete', handleRouteChange)
-
-		// If the component is unmounted, unsubscribe
-		// from the event with the `off` method
-		return () => {
-			router.events.off('routeChangeComplete', handleRouteChange)
-		}
-	}, [router.events])
-
 	return (
 		<>
-			{/* Global Site Tag (gtag.js) - Google Analytics */}
-			<Script
-				strategy='afterInteractive'
-				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-			/>
-			<Script
-				id='gtag-init'
-				strategy='afterInteractive'
-				dangerouslySetInnerHTML={{
-					__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-				}}
-			/>
-
 			{isLoginPage ? (
 				<Component {...pageProps} />
 			) : (
