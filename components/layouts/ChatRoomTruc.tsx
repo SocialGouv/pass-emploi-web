804f70d032ee4a89b70b30d19597e5a0d4fa8dce
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2022-01-04 15:02:37 +0100

    refacto firebase encryption

--- components/layouts/ChatRoomTruc.tsx
+++ components/layouts/ChatRoomTruc.tsx
@@ -2,9 +2,10 @@
  * Script qui permet de copier la BDD firebase vers une autre avec le contenu des messages cryptés. Voici les étapes
  * 1- Enlevez temporairement les règles dans firebase
  * 2- Créez une nouvelle collection (au même niveau que l'ancienne) dans firebase et nommez-là "chat"
- * 3- Changez le nom des variables dans ce fichier en fonction du nom de l'ancienne collenction (collectionNameOld) et la nouvelle (collectionNameNew)
+ * 3- Changez le nom des variables dans ce fichier en fonction du nom de l'ancienne collection (collectionNameOld) et la nouvelle (collectionNameNew)
  * 4- Changez la variable encryptionKey pour mettre la bonne clée d'encodage
  * 5- Lancez le projet web 'yarn dev'
+ * 5b- Bien attendre que toutes les requêtes à firebase soient terminées
  * 6- Voilà! Vous devriez avoir une copie de votre ancienne collection avec des données encodées
  * 7- Remettez les règles firebase
  */
@@ -23,52 +24,65 @@ import { ChatCrypto } from 'utils/chat/chatCrypto'
 
 const collectionNameOld = 'staging-chat'
 const collectionNameNew = 'chat'
-const encryptionKey = 'INSERT-KEY-HERE'
 
 type ChatBoxProps = {
   db: Firestore
 }
 
 export default function ChatBox({ db }: ChatBoxProps) {
-  let encodage: ChatCrypto = new ChatCrypto(encryptionKey)
+  let encodage: ChatCrypto = new ChatCrypto()
 
   async function truc() {
     const oldChats = collection(db, collectionNameOld)
     const newChats = collection(db, collectionNameNew)
 
     const querySnapshot = await getDocs(oldChats)
-    querySnapshot.forEach(async (docOld) => {
+    for (const docOld of querySnapshot.docs) {
       const dataDoc = docOld.data()
-      const encryptedData = encodage.encrypt(dataDoc.lastMessageContent)
+      if (dataDoc?.lastMessageContent && !dataDoc.lastMessageIv) {
+        const encryptedData = encodage.encrypt(dataDoc.lastMessageContent)
 
-      //LastMessageContent
-      await setDoc(doc(newChats, docOld.id), {
-        ...dataDoc,
-        lastMessageContent: encryptedData.encryptedText,
-        lastMessageIv: encryptedData.iv,
-      })
+        //LastMessageContent
+        await setDoc(doc(newChats, docOld.id), {
+          ...dataDoc,
+          lastMessageContent: encryptedData.encryptedText,
+          lastMessageIv: encryptedData.iv,
+        })
+      } else {
+        await setDoc(doc(newChats, docOld.id), {
+          ...dataDoc,
+        })
+      }
 
       //Messages
       const oldMessagesChatRef = collection(
         getChatReference(db, docOld.id, collectionNameOld),
         'messages'
       )
-      const newMessagesChatRef = collection(
-        getChatReference(db, docOld.id, collectionNameNew),
-        'messages'
-      )
-      const querySnapshotMessage = await getDocs(oldMessagesChatRef)
-      querySnapshotMessage.forEach(async (messageOld) => {
-        const messageDoc = messageOld.data()
-        const encryptedMessage = encodage.encrypt(messageDoc.content)
+      if (oldMessagesChatRef) {
+        const newMessagesChatRef = collection(
+          getChatReference(db, docOld.id, collectionNameNew),
+          'messages'
+        )
+        const querySnapshotMessage = await getDocs(oldMessagesChatRef)
+        for (const messageOld of querySnapshotMessage.docs) {
+          const messageDoc = messageOld.data()
+          if (messageDoc?.content && !messageDoc.iv) {
+            const encryptedMessage = encodage.encrypt(messageDoc.content)
 
-        await setDoc(doc(newMessagesChatRef, messageOld.id), {
-          ...messageDoc,
-          content: encryptedMessage.encryptedText,
-          iv: encryptedMessage.iv,
-        })
-      })
-    })
+            await setDoc(doc(newMessagesChatRef, messageOld.id), {
+              ...messageDoc,
+              content: encryptedMessage.encryptedText,
+              iv: encryptedMessage.iv,
+            })
+          } else {
+            await setDoc(doc(newMessagesChatRef, messageOld.id), {
+              ...messageDoc,
+            })
+          }
+        }
+      }
+    }
   }
 
   useEffect(() => {
