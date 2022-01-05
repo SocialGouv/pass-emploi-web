9cdba993823bf7223fd943ebfe085200ff3846f7
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2022-01-05 15:13:42 +0100

    fix: retours revue

--- utils/chat/chatCrypto.ts
+++ utils/chat/chatCrypto.ts
@@ -3,7 +3,7 @@ import Base64 from 'crypto-js/enc-base64'
 import Utf8 from 'crypto-js/enc-utf8'
 import WordArray from 'crypto-js/lib-typedarrays'
 
-interface encryptedTextWithIv {
+interface encryptedTextWithInitializationVector {
   encryptedText: string
   iv: string
 }
@@ -14,7 +14,7 @@ export class ChatCrypto {
     this.key = Utf8.parse(process.env.FIREBASE_CRYPT_KEY ?? '')
   }
 
-  encrypt(message: string): encryptedTextWithIv {
+  encrypt(message: string): encryptedTextWithInitializationVector {
     const iv = WordArray.random(16)
     const encrypted = AES.encrypt(message, this.key, { iv })
 
@@ -24,7 +24,7 @@ export class ChatCrypto {
     }
   }
 
-  decrypt(encryptedText: encryptedTextWithIv): string {
+  decrypt(encryptedText: encryptedTextWithInitializationVector): string {
     return AES.decrypt(encryptedText.encryptedText, this.key, {
       iv: Base64.parse(encryptedText.iv),
     }).toString(Utf8)
