f7767ed93d8d5a26d841211055c1f045f53eefe7
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2022-01-06 16:49:02 +0100

    feat: affiche email jeune

--- fixtures/jeune.ts
+++ fixtures/jeune.ts
@@ -5,6 +5,7 @@ export const unJeune = (overrides: Partial<Jeune> = {}): Jeune =>
     id: 'jeune-1',
     firstName: 'Kenji',
     lastName: 'Jirac',
+    email: 'kenji.jirac@email.fr',
     isActivated: false,
     ...overrides,
   } as Jeune)
