0730a7477f4e2acf4b85a502bcf365a96f54caad
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2022-02-10 10:54:46 +0100

    fix: try preventing redos by limiting email regex size

--- utils/isEmailValid.ts
+++ utils/isEmailValid.ts
@@ -1,5 +1,7 @@
 export default function isEmailValid(email: string) {
+  // https://sonarcloud.io/organizations/socialgouv/rules?open=typescript%3AS5852&rule_key=typescript%3AS5852
+  // https://www.regular-expressions.info/email.html
   const mailRegExp =
-    /[-A-Za-z0-9~!$%^&_=+}{'?]+(\.[-A-Za-z0-9~!$%^&_=+}{'?]+)*@[A-Za-z0-9_][-A-za-z0-9_]+(\.[A-Za-z]{2,6}){1,2}/
+    /^[A-Z0-9._%+-]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
   return mailRegExp.test(email)
 }
