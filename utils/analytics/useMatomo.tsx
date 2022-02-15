2856c01a6a9b8148072436d6a2869f965b2f6931
sandra.hage.chehade <sandra.hage.chehade@octo.com>
2022-02-15 15:25:31 +0100

    fix: useMatomo session non requise

--- utils/analytics/useMatomo.tsx
+++ utils/analytics/useMatomo.tsx
@@ -3,31 +3,33 @@ import { track } from 'utils/analytics/matomo'
 import { useSession } from 'next-auth/react'
 import { UserStructure } from 'interfaces/conseiller'
 
-function userStructureDimensionString(loginMode: string | undefined): string {
+function userStructureDimensionString(loginMode: string): string {
   switch (loginMode) {
     case UserStructure.MILO:
       return 'Mission Locale'
     case UserStructure.POLE_EMPLOI:
       return 'Pôle emploi'
-    case UserStructure.PASS_EMPLOI:
-      return 'pass emploi'
   }
-  return ''
+  return 'pass emploi'
 }
 
 function useMatomo(title: string | undefined) {
-  const { data: session } = useSession({ required: true })
+  const { data: session } = useSession({ required: false })
 
   useEffect(() => {
     if (!title) {
       return
     }
 
+    const structure = !session
+      ? 'visiteur'
+      : userStructureDimensionString(session.user.structure)
+
     track({
-      structure: userStructureDimensionString(session?.user.structure),
+      structure: structure,
       customTitle: title,
     })
-  }, [session?.user.structure, title])
+  }, [session, title])
 }
 
 export default useMatomo
