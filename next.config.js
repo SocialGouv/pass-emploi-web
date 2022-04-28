74a65ae1b6887aa13457857b5531e1383d76bceb
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2022-04-28 08:31:20 +0200

    chore: compile with swc

--- next.config.js
+++ next.config.js
@@ -39,4 +39,18 @@ module.exports = {
     locales: ['fr-FR'],
     defaultLocale: 'fr-FR',
   },
+
+  compiler: {
+    // https://nextjs.org/docs/advanced-features/compiler#remove-react-properties
+    reactRemoveProperties: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
