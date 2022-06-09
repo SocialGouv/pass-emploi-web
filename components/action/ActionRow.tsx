2eeff86f3b1a850045da143f3c11ba9da2ec89ae
gabriel.adgeg <gad@octo.com>
2022-06-09 19:20:51 +0200

    chore(684): Adjust UI for array and rows

--- components/action/ActionRow.tsx
+++ components/action/ActionRow.tsx
@@ -11,14 +11,9 @@ import { formatDayDate } from 'utils/date'
 interface ActionRowProps {
   action: Action
   jeuneId: string
-  borderStyle: string
 }
 
-export default function ActionRow({
-  action,
-  jeuneId,
-  borderStyle,
-}: ActionRowProps) {
+export default function ActionRow({ action, jeuneId }: ActionRowProps) {
   return (
     <Link href={`/mes-jeunes/${jeuneId}/actions/${action.id}`}>
       <a
@@ -26,12 +21,9 @@ export default function ActionRow({
         aria-label={`Détail de l'action ${action.content}`}
         className='table-row hover:bg-primary_lighten hover:outline-0 cursor-pointer focus-within:primary_lighten'
       >
-        <div
-          role='cell'
-          className={`table-cell relative pl-4 py-4 ${borderStyle}`}
-        >
+        <div role='cell' className={`table-cell relative pl-4 py-4`}>
           <span className='flex items-center'>
-            <span className='text-ellipsis overflow-hidden max-w-[400px] whitespace-nowrap'>
+            <span className='text-base-medium text-ellipsis overflow-hidden max-w-[400px] whitespace-nowrap'>
               {action.content}
             </span>
             {action.comment && (
@@ -44,20 +36,17 @@ export default function ActionRow({
             )}
           </span>
         </div>
-        <div
-          role='cell'
-          className={`table-cell relative text-primary_darken ${borderStyle}`}
-        >
+        <div role='cell' className={`table-cell relative`}>
           {formatDayDate(new Date(action.creationDate))}
         </div>
-        <div role='cell' className={`table-cell relative ${borderStyle}`}>
+        <div role='cell' className={`table-cell relative`}>
           <span className='flex items-center justify-between'>
             <StatusTag status={action.status} />
             <IconComponent
               name={IconName.ChevronRight}
               focusable='false'
               aria-hidden='true'
-              className='mr-1 w-6 h-6 fill-primary'
+              className='mr-1 w-6 h-6'
             />
           </span>
         </div>
--- components/action/TableauActionsJeune.tsx
+++ components/action/TableauActionsJeune.tsx
@@ -16,8 +16,6 @@ export const TableauActionsJeune = ({
   actions,
   hideTableHead = false,
 }: TableauActionsJeuneProps) => {
-  const borderStyle = 'border-solid border-0 border-b-2 border-b-grey_700'
-
   return (
     <>
       {actions.length === 0 && (
@@ -36,17 +34,14 @@ export const TableauActionsJeune = ({
             role='rowgroup'
             className={`table-header-group ${hideTableHead ? 'sr-only' : ''}`}
           >
-            <div role='row' className='table-row text-xs-medium text-grey_800'>
-              <div
-                role='columnheader'
-                className={`table-cell pl-4 py-4 ${borderStyle}`}
-              >
+            <div role='row' className='table-row text-s-regular'>
+              <div role='columnheader' className={`table-cell pl-4 py-4`}>
                 Intitulé de l&apos;action
               </div>
-              <div role='columnheader' className={`table-cell ${borderStyle}`}>
+              <div role='columnheader' className={`table-cell`}>
                 Créée le
               </div>
-              <div role='columnheader' className={`table-cell ${borderStyle}`}>
+              <div role='columnheader' className={`table-cell`}>
                 Statut
               </div>
             </div>
@@ -54,12 +49,7 @@ export const TableauActionsJeune = ({
 
           <div role='rowgroup' className='table-row-group'>
             {actions.map((action: Action) => (
-              <ActionRow
-                key={action.id}
-                action={action}
-                jeuneId={jeune.id}
-                borderStyle={borderStyle}
-              />
+              <ActionRow key={action.id} action={action} jeuneId={jeune.id} />
             ))}
           </div>
         </div>
