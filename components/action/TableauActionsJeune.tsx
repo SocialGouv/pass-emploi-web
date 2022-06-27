8d35c497aaa23080be2906eb680ec67fb0096a7b
walid.yahia-cherif <walid.yahia-cherif@octo.com>
2022-06-27 14:54:11 +0200

    style: ajout du style

--- components/action/TableauActionsJeune.tsx
+++ components/action/TableauActionsJeune.tsx
@@ -47,9 +47,9 @@ export const TableauActionsJeune = ({
                 aria-expanded={afficherStatut}
               >
                 Statut
-                {afficherStatut && (
+                {true && (
                   <form
-                    className='absolute z-10 bg-blanc rounded-medium shadow-s'
+                    className='absolute z-10 bg-blanc rounded-medium shadow-s p-4 text-base-regular'
                     id='filtres-statut'
                     onBlur={() => console.log('laal')}
                   >
@@ -57,37 +57,41 @@ export const TableauActionsJeune = ({
                       <legend className='sr-only'>
                         Choisir un statut à filtrer
                       </legend>
-                      <label htmlFor='statut-commencee'>
+                      <label htmlFor='statut-commencee' className='flex'>
                         <input
                           type='checkbox'
                           value={StatutAction.Commencee}
                           id='statut-commencee'
+                          className='h-5 w-5'
                         />
-                        Commencée
+                        <span className='pl-5'>Commencée</span>
                       </label>
-                      <label htmlFor='statut-a-realiser'>
+                      <label htmlFor='statut-a-realiser' className='flex pt-8'>
                         <input
                           type='checkbox'
                           value={StatutAction.ARealiser}
                           id='statut-a-realiser'
+                          className=' h-5 w-5'
                         />
-                        À réaliser
+                        <span className='pl-5'>À réaliser</span>
                       </label>
-                      <label htmlFor='statut-terminee'>
+                      <label htmlFor='statut-terminee' className='flex pt-8'>
                         <input
                           type='checkbox'
                           value={StatutAction.Terminee}
                           id='statut-terminee'
+                          className='h-5 w-5'
                         />
-                        Terminée
+                        <span className='pl-5'>Terminée</span>
                       </label>
-                      <label htmlFor='statut-annulee'>
+                      <label htmlFor='statut-annulee' className='flex pt-8'>
                         <input
                           type='checkbox'
                           value={StatutAction.Annulee}
                           id='statut-annulee'
+                          className='h-5 w-5'
                         />
-                        Annulée
+                        <span className='pl-5'>Annulée</span>
                       </label>
                     </fieldset>
                   </form>
