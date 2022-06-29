9327afd8c368c9cdf7a5c150710c5ce27b724c95
Joseph Robert <joseph.robert@octo.com>
2022-06-29 11:52:28 +0200

    refactor(764): Clean les erreurs de typing sur ActionRow.test.tsx

--- tests/components/ActionRow.test.tsx
+++ tests/components/ActionRow.test.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { render } from '@testing-library/react'
 import { screen } from '@testing-library/dom'
+import { render } from '@testing-library/react'
+import React from 'react'
+
 import ActionRow from 'components/action/ActionRow'
-import { StatutAction } from 'interfaces/action'
 import { uneAction } from 'fixtures/action'
+import { StatutAction } from 'interfaces/action'
 
 describe('<ActionRow/>', () => {
   it("devrait afficher les informations des actions d'un jeune", () => {
     const action = uneAction()
-    render(<ActionRow action={action} jeuneId={'1'} borderStyle='' />)
+    render(<ActionRow action={action} jeuneId={'1'} />)
     expect(
       screen.getByText('Identifier ses atouts et ses compétences')
     ).toBeInTheDocument()
@@ -18,19 +19,19 @@ describe('<ActionRow/>', () => {
 
   it("devrait afficher un badge 'Commencée' quand l'action a été commencée", () => {
     const actionCommencee = uneAction({ status: StatutAction.Commencee })
-    render(<ActionRow action={actionCommencee} jeuneId={'1'} borderStyle='' />)
+    render(<ActionRow action={actionCommencee} jeuneId={'1'} />)
     expect(screen.getByText('Commencée')).toBeInTheDocument()
   })
 
   it("devrait afficher un badge 'Terminée' quand l'action est terminée", () => {
     const actionTerminee = uneAction({ status: StatutAction.Terminee })
-    render(<ActionRow action={actionTerminee} jeuneId={'1'} borderStyle='' />)
+    render(<ActionRow action={actionTerminee} jeuneId={'1'} />)
     expect(screen.getByText('Terminée')).toBeInTheDocument()
   })
 
   it('devrait afficher une icône quand il y a un commentaire', () => {
     const actionTerminee = uneAction()
-    render(<ActionRow action={actionTerminee} jeuneId={'1'} borderStyle='' />)
+    render(<ActionRow action={actionTerminee} jeuneId={'1'} />)
     expect(
       screen.getByLabelText("Un commentaire a été ajouté à l'action")
     ).toBeInTheDocument()
