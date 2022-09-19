ae0d674ab2c6c3b6e65ab2d4dfb1d4dca170a356
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2022-09-19 15:44:41 +0200

    chore: extract component IndicateursJeune

--- pages/mes-jeunes/[jeune_id].tsx
+++ pages/mes-jeunes/[jeune_id].tsx
@@ -9,6 +9,7 @@ import { BlocFavoris } from 'components/jeune/BlocFavoris'
 import DeleteJeuneActifModal from 'components/jeune/DeleteJeuneActifModal'
 import DeleteJeuneInactifModal from 'components/jeune/DeleteJeuneInactifModal'
 import { DetailsJeune } from 'components/jeune/DetailsJeune'
+import { IndicateursJeune } from 'components/jeune/IndicateursJeune'
 import { OngletRdvs } from 'components/rdv/OngletRdvs'
 import ButtonLink from 'components/ui/Button/ButtonLink'
 import IconComponent, { IconName } from 'components/ui/IconComponent'
@@ -16,7 +17,6 @@ import Tab from 'components/ui/Navigation/Tab'
 import TabList from 'components/ui/Navigation/TabList'
 import FailureAlert from 'components/ui/Notifications/FailureAlert'
 import InformationMessage from 'components/ui/Notifications/InformationMessage'
-import TileIndicateur from 'components/ui/TileIndicateurComponent'
 import {
   Action,
   EtatQualificationAction,
@@ -309,68 +309,11 @@ function FicheJeune({
       </div>
 
       {!isPoleEmploi && (
-        <div className='border border-solid rounded-medium w-full p-4 border-grey_100'>
-          <h2 className='text-m-bold'>Les indicateurs de la semaine</h2>
-          <p className='mb-4'>
-            du {debutDeLaSemaine.toLocaleString()} au{' '}
-            {finDeLaSemaine.toLocaleString()}
-          </p>
-          <div
-            className={`flex gap-6 ${
-              !indicateursSemaine ? 'animate-pulse' : ''
-            }`}
-          >
-            <div className='text-primary_darken text-base-bold'>
-              <h3 className='mb-2'>Les actions</h3>
-              <ul className='flex gap-2'>
-                <TileIndicateur
-                  valeur={indicateursSemaine?.actions.creees.toString() ?? '-'}
-                  label={
-                    indicateursSemaine?.actions.creees !== 1
-                      ? 'Créées'
-                      : 'Créée'
-                  }
-                  bgColor='primary_lighten'
-                  textColor='primary_darken'
-                />
-                <TileIndicateur
-                  valeur={
-                    indicateursSemaine?.actions.terminees.toString() ?? '-'
-                  }
-                  label={
-                    indicateursSemaine?.actions.terminees !== 1
-                      ? 'Terminées'
-                      : 'Terminée'
-                  }
-                  bgColor='accent_2_lighten'
-                  textColor='accent_2'
-                  iconName={IconName.RoundedCheck}
-                />
-                <TileIndicateur
-                  valeur={
-                    indicateursSemaine?.actions.enRetard.toString() ?? '-'
-                  }
-                  label='En retard'
-                  bgColor='alert_lighten'
-                  textColor='content_color'
-                  iconName={IconName.WarningRounded}
-                />
-              </ul>
-            </div>
-            <div className='text-primary_darken text-base-bold'>
-              <h3 className='mb-2'>Les rendez-vous</h3>
-              <div className='flex gap-2'>
-                <TileIndicateur
-                  valeur={indicateursSemaine?.rendezVous.toString() ?? '-'}
-                  label='Cette semaine'
-                  bgColor='primary_lighten'
-                  textColor='primary_darken'
-                />
-              </div>
-            </div>
-            <div></div>
-          </div>
-        </div>
+        <IndicateursJeune
+          debutDeLaSemaine={debutDeLaSemaine}
+          finDeLaSemaine={finDeLaSemaine}
+          indicateursSemaine={indicateursSemaine}
+        />
       )}
 
       <div className='flex justify-between mt-6 mb-4'>
