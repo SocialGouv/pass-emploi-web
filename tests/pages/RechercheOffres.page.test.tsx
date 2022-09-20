1930cc8ac35234c46816e704d4c16b467988a4f5
walid.yahia-cherif <walid.yahia-cherif@octo.com>
2022-09-20 16:37:02 +0200

    feat: ajout du titre de  la page

--- tests/pages/RechercheOffres.page.test.tsx
+++ tests/pages/RechercheOffres.page.test.tsx
@@ -1,6 +1,11 @@
 import { render, screen } from '@testing-library/react'
 
 import RechercheOffres from 'pages/recherche-offres'
+import { withMandatorySessionOrRedirect } from 'utils/auth/withMandatorySessionOrRedirect'
+import { getServerSideProps } from 'pages/recherche-offres'
+import { GetServerSidePropsContext } from 'next/types'
+
+jest.mock('utils/auth/withMandatorySessionOrRedirect')
 
 jest.mock('utils/auth/withMandatorySessionOrRedirect')
 
@@ -22,6 +27,41 @@ describe('Page Recherche Offres', () => {
 
       // Then
       expect(screen.getByLabelText(/Mots clés/)).toHaveAttribute('type', 'text')
+    })
+  })
+
+  describe('server side', () => {
+    it('requiert la connexion', async () => {
+      // Given
+      ;(withMandatorySessionOrRedirect as jest.Mock).mockResolvedValue({
+        validSession: false,
+        redirect: { destination: 'whatever' },
+      })
+
+      // When
+      const actual = await getServerSideProps({} as GetServerSidePropsContext)
+
+      // Then
+      expect(withMandatorySessionOrRedirect).toHaveBeenCalled()
+      expect(actual).toEqual({ redirect: { destination: 'whatever' } })
+    })
+
+    it('charge la page avec les bonnes props', async () => {
+      // Given
+      ;(withMandatorySessionOrRedirect as jest.Mock).mockResolvedValue({
+        validSession: true,
+      })
+
+      // When
+      const actual = await getServerSideProps({} as GetServerSidePropsContext)
+
+      // Then
+      expect(actual).toEqual({
+        props: {
+          pageTitle: 'Recherche d’offres',
+          pageHeader: 'Offres',
+        },
+      })
     })
   })
 })
