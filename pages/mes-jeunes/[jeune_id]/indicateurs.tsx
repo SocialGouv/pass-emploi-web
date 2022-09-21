369c1c5ca336610632bebc42908ee63980494055
gabriel.adgeg <gad@octo.com>
2022-09-21 14:59:20 +0200

    chore(1023) : ajout de toutes les informations au layout

--- pages/mes-jeunes/[jeune_id]/indicateurs.tsx
+++ pages/mes-jeunes/[jeune_id]/indicateurs.tsx
@@ -3,7 +3,8 @@ import { DateTime } from 'luxon'
 import { GetServerSideProps } from 'next'
 import React, { useEffect, useState } from 'react'
 
-import { IndicateursJeune } from 'components/jeune/IndicateursJeune'
+import { IconName } from 'components/ui/IconComponent'
+import TileIndicateur from 'components/ui/TileIndicateur'
 import { StructureConseiller } from 'interfaces/conseiller'
 import { IndicateursSemaine } from 'interfaces/jeune'
 import { PageProps } from 'interfaces/pageProps'
@@ -50,11 +51,113 @@ function Indicateurs({ idJeune, idConseiller }: IndicateursProps) {
   ])
 
   return (
-    <IndicateursJeune
-      debutDeLaSemaine={debutDeLaSemaine}
-      finDeLaSemaine={finDeLaSemaine}
-      indicateursSemaine={indicateursSemaine}
-    />
+    <div>
+      <h2 className='text-m-bold text-content_color mb-6 mt-2'>
+        Semaine du {debutDeLaSemaine.toLocaleString()} au{' '}
+        {finDeLaSemaine.toLocaleString()}
+      </h2>
+
+      <div className='border border-solid rounded-medium w-full p-4 border-grey_100'>
+        <h3 className='text-m-bold text-content_color mb-4'>Les actions</h3>
+        <ul className='flex gap-2'>
+          <TileIndicateur
+            valeur={indicateursSemaine?.actions.creees.toString() ?? '-'}
+            label={
+              indicateursSemaine?.actions.creees !== 1 ? 'Créées' : 'Créée'
+            }
+            bgColor='primary_lighten'
+            textColor='primary_darken'
+          />
+          <TileIndicateur
+            valeur={indicateursSemaine?.actions.enRetard.toString() ?? '-'}
+            label='En retard'
+            bgColor='alert_lighten'
+            textColor='content_color'
+            iconName={IconName.WarningRounded}
+          />
+          <TileIndicateur
+            valeur={indicateursSemaine?.actions.terminees.toString() ?? '-'}
+            label={
+              indicateursSemaine?.actions.terminees !== 1
+                ? 'Terminées'
+                : 'Terminée'
+            }
+            bgColor='accent_2_lighten'
+            textColor='accent_2'
+            iconName={IconName.RoundedCheck}
+          />
+          <TileIndicateur
+            valeur={indicateursSemaine?.actions.aEcheance.toString() ?? '-'}
+            label='Échéance cette semaine'
+            bgColor='primary_lighten'
+            textColor='primary_darken'
+          />
+        </ul>
+      </div>
+
+      <div className='border border-solid rounded-medium w-full mt-6 p-4 border-grey_100'>
+        <h3 className='text-m-bold text-content_color mb-4'>Les rendez-vous</h3>
+        <ul className='flex'>
+          <TileIndicateur
+            valeur={indicateursSemaine?.rendezVous.toString() ?? '-'}
+            label='Cette semaine'
+            bgColor='primary_lighten'
+            textColor='primary_darken'
+          />
+        </ul>
+      </div>
+
+      <div className='border border-solid rounded-medium w-full mt-6 p-4 border-grey_100'>
+        <h3 className='text-m-bold text-content_color mb-4'>Les offres</h3>
+        <ul className='flex gap-2'>
+          <TileIndicateur
+            valeur={indicateursSemaine?.offres.consultees.toString() ?? '-'}
+            label={
+              indicateursSemaine?.offres.consultees !== 1
+                ? 'Offres consultées'
+                : 'Offre consultée'
+            }
+            bgColor='primary_lighten'
+            textColor='primary_darken'
+          />
+          <TileIndicateur
+            valeur={
+              indicateursSemaine?.favoris.offresSauvegardees.toString() ?? '-'
+            }
+            label={
+              indicateursSemaine?.favoris.offresSauvegardees !== 1
+                ? 'Favoris ajoutés'
+                : 'Favori ajouté'
+            }
+            bgColor='primary_lighten'
+            textColor='primary_darken'
+          />
+          <TileIndicateur
+            valeur={indicateursSemaine?.offres.partagees.toString() ?? '-'}
+            label={
+              indicateursSemaine?.offres.partagees !== 1
+                ? 'Offres partagées'
+                : 'Offre partagée'
+            }
+            bgColor='primary_lighten'
+            textColor='primary_darken'
+          />
+          <TileIndicateur
+            valeur={
+              indicateursSemaine?.favoris.recherchesSauvegardees.toString() ??
+              '-'
+            }
+            label={
+              indicateursSemaine?.favoris.recherchesSauvegardees !== 1
+                ? 'Recherches sauvegardées'
+                : 'Recherche sauvegardée'
+            }
+            bgColor='primary_lighten'
+            textColor='primary_darken'
+          />
+        </ul>
+      </div>
+    </div>
   )
 }
 
