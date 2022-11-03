e961284c59207131be9cfdeb7acd2a35a9bb673c
gabriel.adgeg <gad@octo.com>
2022-11-03 17:38:35 +0100

    chore(1027): desérialization d'un objet au bon type SearchOffresEmploiQuery

--- pages/offres/partage-critere.tsx
+++ pages/offres/partage-critere.tsx
@@ -1,15 +1,30 @@
 import { withTransaction } from '@elastic/apm-rum-react'
 import { GetServerSideProps } from 'next'
 
+import { TypeOffre } from 'interfaces/offre'
 import { PageProps } from 'interfaces/pageProps'
+import { SearchOffresEmploiQuery } from 'services/offres-emploi.service'
 import { withMandatorySessionOrRedirect } from 'utils/auth/withMandatorySessionOrRedirect'
 
 type PartageCritereProps = PageProps & {
-  query: string
+  type: TypeOffre
+  criteresDeRecherche: SearchOffresEmploiQuery
 }
 
-function PartageCritere({ query }: PartageCritereProps) {
-  return <>{query}</>
+function PartageCritere({ criteresDeRecherche }: PartageCritereProps) {
+  return (
+    <div>
+      {criteresDeRecherche.motsCles +
+        ' ' +
+        criteresDeRecherche.durees +
+        ' ' +
+        criteresDeRecherche.typesContrats +
+        ' ' +
+        criteresDeRecherche.commune?.libelle +
+        ' ' +
+        criteresDeRecherche.departement}
+    </div>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps<
@@ -20,14 +35,27 @@ export const getServerSideProps: GetServerSideProps<
     return { redirect: sessionOrRedirect.redirect }
   }
 
-  const q = context.query.criteres as string
+  const criteresEncoded = context.query.criteres as string
+  const buff = new Buffer(criteresEncoded, 'base64')
+  const criteresDecoded = buff.toString('utf-8')
 
-  const buff = new Buffer(q, 'base64')
-  const queryParams = buff.toString('utf-8')
+  const queryOffresEmploiJson = JSON.parse(criteresDecoded)
+
+  const queryOffresEmploi: SearchOffresEmploiQuery = {
+    ...queryOffresEmploiJson,
+    //TODO-1027 Objet vide ou undefined ?
+    departement: queryOffresEmploiJson.departement
+      ? JSON.parse(queryOffresEmploiJson.departement)
+      : {},
+    commune: queryOffresEmploiJson.commune
+      ? JSON.parse(queryOffresEmploiJson.commune)
+      : {},
+  }
 
   const props: PartageCritereProps = {
     pageTitle: 'Partager une offre',
-    query: queryParams,
+    type: context.query.type as TypeOffre,
+    criteresDeRecherche: queryOffresEmploi,
   }
 
   return { props }
