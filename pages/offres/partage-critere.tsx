d25b3f6350f747603406bd2829b4ca12fec342b1
gabriel.adgeg <gad@octo.com>
2022-11-03 18:15:26 +0100

    chore(1027): ajout des premiers éléments du formulaire

--- pages/offres/partage-critere.tsx
+++ pages/offres/partage-critere.tsx
@@ -1,29 +1,107 @@
 import { withTransaction } from '@elastic/apm-rum-react'
 import { GetServerSideProps } from 'next'
+import { useRouter } from 'next/router'
+import React, { FormEvent, useMemo, useState } from 'react'
 
+import JeunesMultiselectAutocomplete from 'components/jeune/JeunesMultiselectAutocomplete'
+import { RequiredValue } from 'components/RequiredValue'
+import Button, { ButtonStyle } from 'components/ui/Button/Button'
+import ButtonLink from 'components/ui/Button/ButtonLink'
+import { Etape } from 'components/ui/Form/Etape'
+import IconComponent, { IconName } from 'components/ui/IconComponent'
+import { BaseJeune } from 'interfaces/jeune'
 import { TypeOffre } from 'interfaces/offre'
 import { PageProps } from 'interfaces/pageProps'
+import { QueryParam, QueryValue } from 'referentiel/queryParam'
+import { JeunesService } from 'services/jeunes.service'
 import { SearchOffresEmploiQuery } from 'services/offres-emploi.service'
 import { withMandatorySessionOrRedirect } from 'utils/auth/withMandatorySessionOrRedirect'
+import withDependance from 'utils/injectionDependances/withDependance'
 
 type PartageCritereProps = PageProps & {
+  jeunes: BaseJeune[]
   type: TypeOffre
   criteresDeRecherche: SearchOffresEmploiQuery
+  withoutChat: true
+  returnTo: string
 }
 
-function PartageCritere({ criteresDeRecherche }: PartageCritereProps) {
+function PartageCritere({
+  jeunes,
+  type,
+  criteresDeRecherche,
+  returnTo,
+}: PartageCritereProps) {
+  const router = useRouter()
+  const [idsDestinataires, setIdsDestinataires] = useState<
+    RequiredValue<string[]>
+  >({ value: [] })
+  const [isPartageEnCours, setIsPartageEnCours] = useState<boolean>(false)
+
+  const formIsValid = useMemo(
+    () => idsDestinataires.value.length > 0,
+    [idsDestinataires]
+  )
+
+  function updateIdsDestinataires(selectedIds: string[]) {
+    setIdsDestinataires({
+      value: selectedIds,
+      error: !selectedIds.length
+        ? "Aucun bénéficiaire n'est renseigné. Veuillez sélectionner au moins un bénéficiaire."
+        : undefined,
+    })
+  }
+
+  async function partager(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    if (!formIsValid) return
+
+    setIsPartageEnCours(true)
+
+    try {
+      // TODO-1027 appel service
+      await router.push({
+        pathname: returnTo,
+        query: { [QueryParam.partageOffre]: QueryValue.succes },
+      })
+    } finally {
+      setIsPartageEnCours(false)
+    }
+  }
+
   return (
-    <div>
-      {criteresDeRecherche.motsCles +
-        ' ' +
-        criteresDeRecherche.durees +
-        ' ' +
-        criteresDeRecherche.typesContrats +
-        ' ' +
-        criteresDeRecherche.commune?.libelle +
-        ' ' +
-        criteresDeRecherche.departement}
-    </div>
+    <>
+      <form onSubmit={partager} className='mt-8'></form>
+      <Etape numero={1} titre='Bénéficiaires'>
+        <JeunesMultiselectAutocomplete
+          jeunes={jeunes}
+          typeSelection='Bénéficiaires'
+          onUpdate={updateIdsDestinataires}
+          error={idsDestinataires.error}
+        />
+      </Etape>
+
+      <div className='flex justify-center'>
+        <ButtonLink href={returnTo} style={ButtonStyle.SECONDARY}>
+          Annuler
+        </ButtonLink>
+
+        <Button
+          type='submit'
+          className='ml-3 flex items-center'
+          disabled={!formIsValid}
+          isLoading={isPartageEnCours}
+        >
+          <IconComponent
+            name={IconName.Send}
+            aria-hidden={true}
+            focusable={false}
+            className='mr-2 h-4 w-4 fill-blanc'
+          />
+          Envoyer
+        </Button>
+      </div>
+    </>
   )
 }
 
@@ -34,6 +112,13 @@ export const getServerSideProps: GetServerSideProps<
   if (!sessionOrRedirect.validSession) {
     return { redirect: sessionOrRedirect.redirect }
   }
+
+  const { user, accessToken } = sessionOrRedirect.session
+  const jeunesService = withDependance<JeunesService>('jeunesService')
+  const jeunes = await jeunesService.getJeunesDuConseillerServerSide(
+    user.id,
+    accessToken
+  )
 
   const criteresEncoded = context.query.criteres as string
   const buff = new Buffer(criteresEncoded, 'base64')
@@ -52,13 +137,18 @@ export const getServerSideProps: GetServerSideProps<
       : {},
   }
 
-  const props: PartageCritereProps = {
-    pageTitle: 'Partager une offre',
-    type: context.query.type as TypeOffre,
-    criteresDeRecherche: queryOffresEmploi,
+  const referer = context.req.headers.referer
+  const redirectTo = referer ?? '/recherche-offres'
+  return {
+    props: {
+      jeunes,
+      type: context.query.type as TypeOffre,
+      criteresDeRecherche: queryOffresEmploi,
+      returnTo: redirectTo,
+      withoutChat: true,
+      pageTitle: 'Partager une offre',
+    },
   }
-
-  return { props }
 }
 
 export default withTransaction(PartageCritere.name, 'page')(PartageCritere)
