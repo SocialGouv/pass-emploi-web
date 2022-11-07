840db5aea754379f00998ab2a8e28c80a450f892
gabriel.adgeg <gad@octo.com>
2022-11-07 19:08:34 +0100

    refactor(1027): rend la SuggestionCard.tsx plus générique à tous les types d'offres

--- components/offres/suggestions/SuggestionCard.tsx
+++ components/offres/suggestions/SuggestionCard.tsx
@@ -2,22 +2,36 @@ import React from 'react'
 
 import { IconName } from 'components/ui/IconComponent'
 import { DataTag } from 'components/ui/Indicateurs/DataTag'
+import { TypeOffre } from 'interfaces/offre'
 
-interface SuggestionOffresEmploiCardProps {
+interface SuggestionCardProps {
+  type: TypeOffre
   titre: string
   motsCles: string
   labelLocalite: string
 }
 
-export default function SuggestionOffresEmploiCard({
+export default function SuggestionCard({
+  type,
   titre,
   motsCles,
   labelLocalite,
-}: SuggestionOffresEmploiCardProps) {
+}: SuggestionCardProps) {
+  function getTypeLabel(): string {
+    switch (type) {
+      case TypeOffre.EMPLOI:
+        return 'Offre d’emploi'
+      case TypeOffre.SERVICE_CIVIQUE:
+      case TypeOffre.IMMERSION:
+      case TypeOffre.ALTERNANCE:
+        return ''
+    }
+  }
+
   return (
     <div className='rounded-small shadow-s p-6'>
       <p className='text-base-bold mb-2'>{titre}</p>
-      <DataTag text={'Offre d’emploi'} />
+      <DataTag text={getTypeLabel()} />
       <DataTag className='ml-2' text={motsCles} />
       {/* TODO-1027 autre moyen pour passer à la ligne que d'encapsuler dans div ? */}
       <div className='mt-4'>
