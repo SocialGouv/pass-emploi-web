3cec818e8e0ec0068e7e8d1ab8f0af540e2558df
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2022-12-14 12:10:16 +0100

    feat: affiche message quand aucune liste de diffusion

--- components/chat/ChatsDeDiffusion.tsx
+++ components/chat/ChatsDeDiffusion.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import EmptyStateImage from 'assets/images/empty_state.svg'
 import IconComponent, { IconName } from 'components/ui/IconComponent'
 import { SpinningLoader } from 'components/ui/SpinningLoader'
 import { ListeDeDiffusion } from 'interfaces/liste-de-diffusion'
@@ -14,52 +15,64 @@ export default function ChatsDeDiffusion({
   onBack,
 }: ChatsDeDiffusionProps) {
   return (
-    <>
-      {!listesDeDiffusion && <SpinningLoader />}
-      {listesDeDiffusion && (
-        <div className='h-full flex flex-col bg-grey_100 '>
-          <div className='flex items-center mx-4 pb-6 my-6 border-b border-grey_500 short:hidden'>
-            <button
-              className='p-3 border-none rounded-full mr-2 bg-primary_lighten'
-              onClick={onBack}
-            >
-              <IconComponent
-                name={IconName.ChevronLeft}
-                role='img'
-                focusable='false'
-                aria-label='Retour sur ma messagerie'
-                className='w-6 h-6 fill-[fillPrimary]'
-              />
-            </button>
-            <h2 className='w-full text-left text-primary text-l-bold'>
-              Mes listes de diffusion
-            </h2>
-          </div>
+    <div className='h-full flex flex-col bg-grey_100 '>
+      <div className='flex items-center mx-4 pb-6 my-6 border-b border-grey_500 short:hidden'>
+        <button
+          className='p-3 border-none rounded-full mr-2 bg-primary_lighten'
+          onClick={onBack}
+        >
+          <IconComponent
+            name={IconName.ChevronLeft}
+            role='img'
+            focusable='false'
+            aria-label='Retour sur ma messagerie'
+            className='w-6 h-6 fill-[fillPrimary]'
+          />
+        </button>
+        <h2 className='w-full text-left text-primary text-l-bold'>
+          Mes listes de diffusion
+        </h2>
+      </div>
 
-          <div className='px-4 overflow-y-auto'>
-            <h3
-              id='listes-de-diffusion'
-              className='text-m-medium text-primary mb-4'
-            >
-              Listes ({listesDeDiffusion.length})
-            </h3>
-            <ul aria-describedby='listes-de-diffusion'>
-              {listesDeDiffusion.map((liste) => (
-                <li
-                  key={Math.random()}
-                  className='p-3 bg-blanc rounded-small mb-2 last:mb-0'
-                >
-                  <TitreListe liste={liste} />
-                  <span className='text-s-regular'>
-                    {liste.beneficiaires.length} destinataire(s)
-                  </span>
-                </li>
-              ))}
-            </ul>
-          </div>
+      {!listesDeDiffusion && <SpinningLoader />}
+
+      {listesDeDiffusion && listesDeDiffusion.length === 0 && (
+        <div className='bg-grey_100 flex flex-col justify-center items-center'>
+          <EmptyStateImage
+            focusable='false'
+            aria-hidden='true'
+            className='w-[360px] h-[200px]'
+          />
+          <p className='mt-4 text-base-medium w-2/3 text-center'>
+            Vous n’avez pas encore créé de liste de diffusion
+          </p>
         </div>
       )}
-    </>
+
+      {listesDeDiffusion && listesDeDiffusion.length > 0 && (
+        <div className='px-4 overflow-y-auto'>
+          <h3
+            id='listes-de-diffusion'
+            className='text-m-medium text-primary mb-4'
+          >
+            Listes ({listesDeDiffusion.length})
+          </h3>
+          <ul aria-describedby='listes-de-diffusion'>
+            {listesDeDiffusion.map((liste) => (
+              <li
+                key={Math.random()}
+                className='p-3 bg-blanc rounded-small mb-2 last:mb-0'
+              >
+                <TitreListe liste={liste} />
+                <span className='text-s-regular'>
+                  {liste.beneficiaires.length} destinataire(s)
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
   )
 }
 
--- tests/components/ChatsDeDiffusion.test.tsx
+++ tests/components/ChatsDeDiffusion.test.tsx
@@ -7,49 +7,62 @@ import { ListeDeDiffusion } from 'interfaces/liste-de-diffusion'
 
 describe('<ChatsDeDiffusion />', () => {
   let listesDeDiffusion: ListeDeDiffusion[]
-  beforeEach(async () => {
-    // Given
-    listesDeDiffusion = desListesDeDiffusion()
 
-    // When
-    render(
-      <ChatsDeDiffusion
-        listesDeDiffusion={listesDeDiffusion}
-        onBack={() => {}}
-      />
-    )
-  })
+  describe('quand le conseiller a des listes de diffusion', () => {
+    beforeEach(async () => {
+      // Given
+      listesDeDiffusion = desListesDeDiffusion()
 
-  it('affiche les listes de diffusion', async () => {
-    // Then
-    const listes = screen.getByRole('list', {
-      description: 'Listes (2)',
+      // When
+      render(
+        <ChatsDeDiffusion
+          listesDeDiffusion={listesDeDiffusion}
+          onBack={() => {}}
+        />
+      )
     })
-    expect(within(listes).getAllByRole('listitem')).toHaveLength(
-      listesDeDiffusion.length
-    )
-    listesDeDiffusion.forEach((liste) => {
-      const titreListe = within(listes).getByRole('heading', {
-        level: 4,
-        name: new RegExp(liste.titre),
-      })
 
-      expect(titreListe).toBeInTheDocument()
-      expect(titreListe.nextElementSibling).toHaveTextContent(
-        liste.beneficiaires.length + ' destinataire(s)'
+    it('affiche les listes de diffusion', async () => {
+      // Then
+      const listes = screen.getByRole('list', {
+        description: 'Listes (2)',
+      })
+      expect(within(listes).getAllByRole('listitem')).toHaveLength(
+        listesDeDiffusion.length
+      )
+      listesDeDiffusion.forEach((liste) => {
+        const titreListe = within(listes).getByRole('heading', {
+          level: 4,
+          name: new RegExp(liste.titre),
+        })
+
+        expect(titreListe).toBeInTheDocument()
+        expect(titreListe.nextElementSibling).toHaveTextContent(
+          liste.beneficiaires.length + ' destinataire(s)'
+        )
+      })
+    })
+
+    it('prévient quand une liste contient un bénéficiaire transféré temporairement', async () => {
+      // Then
+      expect(
+        screen.getByRole('heading', {
+          level: 4,
+          name:
+            'Un ou plusieurs bénéficiaires de cette liste ont été réaffectés temporairement. ' +
+            listesDeDiffusion[1].titre,
+        })
       )
     })
   })
 
-  it('prévient quand une liste contient un bénéficiaire transféré temporairement', async () => {
+  describe('quand le conseiller n‘a pas de liste de diffusion', () => {
+    // When
+    render(<ChatsDeDiffusion listesDeDiffusion={[]} onBack={() => {}} />)
+
     // Then
     expect(
-      screen.getByRole('heading', {
-        level: 4,
-        name:
-          'Un ou plusieurs bénéficiaires de cette liste ont été réaffectés temporairement. ' +
-          listesDeDiffusion[1].titre,
-      })
-    )
+      screen.getByText('Vous n’avez pas encore créé de liste de diffusion')
+    ).toBeInTheDocument()
   })
 })
