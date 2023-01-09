092a7ffbc1f805d4fd7a24cfe8e72cd000149f8b
Amelle El Bakkal <amelle.el.bakkal@octo.com>
2023-01-09 15:31:19 +0100

    feat(US-995): Met l'agenda du lundi au dimanche

--- components/agenda-jeune/EntreesAgendaParJourDeLaSemaine.tsx
+++ components/agenda-jeune/EntreesAgendaParJourDeLaSemaine.tsx
@@ -7,8 +7,6 @@ import { EntreeAgenda } from 'interfaces/agenda'
 export type SemaineAgenda = {
   jours: { [jour: string]: { date: DateTime; entrees: EntreeAgenda[] } }
   aEvenement: boolean
-  afficherSamedi: boolean
-  afficherDimanche: boolean
 }
 
 type EntreesAgendaParJourDeLaSemaineProps = {
@@ -21,44 +19,37 @@ export function EntreesAgendaParJourDeLaSemaine({
   numeroSemaine,
   semaine,
 }: EntreesAgendaParJourDeLaSemaineProps) {
-  function afficherDate(date: DateTime) {
-    if (semaine.afficherSamedi) return true
-    if (semaine.afficherDimanche && date.weekday === 7) return true
-    return date.weekday <= 5
-  }
-
   return (
     <>
       {Object.entries(semaine.jours).map(([jour, { date, entrees }], index) => {
-        if (afficherDate(date))
-          return (
-            <section
-              key={`semaine-${numeroSemaine}-jour-${index}`}
-              aria-labelledby={`semaine-${numeroSemaine}-jour-${index}`}
-              className='rounded-small border border-grey_100 p-4 mt-6'
+        return (
+          <section
+            key={`semaine-${numeroSemaine}-jour-${index}`}
+            aria-labelledby={`semaine-${numeroSemaine}-jour-${index}`}
+            className='rounded-small border border-grey_100 p-4 mt-6'
+          >
+            <h3
+              id={`semaine-${numeroSemaine}-jour-${index}`}
+              className='text-base-medium mb-2'
             >
-              <h3
-                id={`semaine-${numeroSemaine}-jour-${index}`}
-                className='text-base-medium mb-2'
-              >
-                {capitalizeFirstLetter(jour)}
-              </h3>
-              {entrees.length === 0 && (
-                <p className='text-grey_800'>Pas d’action ni de rendez-vous</p>
-              )}
-              {entrees.length > 0 && (
-                <ol>
-                  {entrees.map((entree) => (
-                    <AgendaRow
-                      key={entree.id}
-                      entree={entree}
-                      jeuneId={idBeneficiaire}
-                    />
-                  ))}
-                </ol>
-              )}
-            </section>
-          )
+              {capitalizeFirstLetter(jour)}
+            </h3>
+            {entrees.length === 0 && (
+              <p className='text-grey_800'>Pas d’action ni de rendez-vous</p>
+            )}
+            {entrees.length > 0 && (
+              <ol>
+                {entrees.map((entree) => (
+                  <AgendaRow
+                    key={entree.id}
+                    entree={entree}
+                    jeuneId={idBeneficiaire}
+                  />
+                ))}
+              </ol>
+            )}
+          </section>
+        )
       })}
     </>
   )
