b6d9617bbbd66ffee2101da9745ec7be40c9445e
Theo Gautier <theo.gautier@octo.com>
2023-06-05 15:40:03 +0200

    clean SW pour chaque utilisateur

---
+++ public/unregisterServiceWorker.js
@@ -0,0 +1,12 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister()
+      }
+    })
+    .catch(function (err) {
+      console.log('Service Worker registration failed: ', err)
+    })
+}
