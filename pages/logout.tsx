9150504a7a12fbd1e5b6e3e6adc7503e5d132c7b
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2023-06-06 11:43:26 +0200

    fix: corrige erreur firebase on logout

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,27 +1,15 @@
 import { withTransaction } from '@elastic/apm-rum-react'
-import { useRouter } from 'next/router'
 import { signOut } from 'next-auth/react'
 import React, { useEffect } from 'react'
 
 import { signOut as chatSignOut } from 'services/messages.service'
 
 function Logout() {
-  const router = useRouter()
-
   useEffect(() => {
-    async function logout() {
-      try {
-        await Promise.all([
-          chatSignOut(),
-          signOut({ redirect: false, callbackUrl: '/login' }),
-        ])
-      } finally {
-        router.push('/login')
-      }
-    }
-
-    logout()
-  }, [router])
+    chatSignOut().then(() => {
+      signOut({ redirect: true, callbackUrl: '/login' })
+    })
+  }, [])
 
   return null
 }
