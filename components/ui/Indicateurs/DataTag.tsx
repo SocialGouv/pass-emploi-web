a15b210a18aefbbfc44636e43b477b85af94275c
Amelle El Bakkal <amelle.el.bakkal@octo.com>
2023-08-25 12:20:15 +0200

    fix(US-1868): Corrige background datatag

--- components/ui/Indicateurs/DataTag.tsx
+++ components/ui/Indicateurs/DataTag.tsx
@@ -4,7 +4,7 @@ import IconComponent, { IconName } from 'components/ui/IconComponent'
 
 interface DataTagProps {
   text: string
-  style?: 'primary' | 'additional'
+  style?: 'primary' | 'secondary' | 'additional'
   iconName?: IconName
   className?: string
   iconLabel?: string
@@ -17,15 +17,31 @@ export function DataTag({
   className,
   iconLabel,
 }: DataTagProps) {
-  const color = style === 'primary' ? 'text-primary' : 'text-content_color'
-  const background =
-    style === 'primary' ? 'bg-primary_lighten' : 'bg-additional_5_lighten'
+  function getBackground(): string {
+    switch (style) {
+      case 'primary':
+        return 'bg-primary_lighten'
+      case 'secondary':
+        return 'bg-blanc'
+      case 'additional':
+        return 'bg-additional_5_lighten'
+    }
+  }
 
+  function getTextColor(): string {
+    switch (style) {
+      case 'primary':
+      case 'secondary':
+        return 'text-primary'
+      case 'additional':
+        return 'text-content_color'
+    }
+  }
   return (
     <span
       className={`inline-flex items-center rounded-base ${
         iconName ? 'px-2' : 'px-4'
-      } py-1 text-s-medium ${color} ${background} whitespace-nowrap ${
+      } py-1 text-s-medium ${getTextColor()} ${getBackground()} whitespace-nowrap ${
         className ?? ''
       }`}
     >
