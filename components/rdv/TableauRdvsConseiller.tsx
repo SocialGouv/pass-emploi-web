9db33626ada2f5a64550d3939c228d2098b50899
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2023-09-21 17:21:01 +0200

    fix: remove empty state agenda conseiller

--- components/rdv/TableauRdvsConseiller.tsx
+++ components/rdv/TableauRdvsConseiller.tsx
@@ -1,10 +1,7 @@
 import { DateTime } from 'luxon'
 import React from 'react'
 
-import EmptyState from 'components/EmptyState'
 import { RdvRow } from 'components/rdv/RdvRow'
-import { IconName } from 'components/ui/IconComponent'
-import { IllustrationName } from 'components/ui/IllustrationComponent'
 import Table from 'components/ui/Table/Table'
 import { TBody } from 'components/ui/Table/TBody'
 import { TH } from 'components/ui/Table/TH'
@@ -29,49 +26,26 @@ export default function TableauRdvsConseiller({
   )
 
   return (
-    <>
-      {rdvs.length === 0 && (
-        <div className='flex flex-col justify-center items-center'>
-          <EmptyState
-            illustrationName={IllustrationName.Checklist}
-            titre='Vous n’avez rien de prévu pour l’instant.'
-            premierLien={{
-              href: '/mes-jeunes/edition-rdv?type=ac',
-              label: 'Créer une animation collective',
-              iconName: IconName.Add,
-            }}
-            secondLien={{
-              href: '/mes-jeunes/edition-rdv',
-              label: 'Créer un rendez-vous',
-              iconName: IconName.Add,
-            }}
-          />
-        </div>
-      )}
+    <Table asDiv={true} caption={{ text: 'Liste de mes événements' }}>
+      <THead>
+        <TR isHeader={true}>
+          <TH>Horaires</TH>
+          <TH>Bénéficiaire</TH>
+          <TH>Type</TH>
+          <TH>Modalité</TH>
+          <TH>Créé par vous</TH>
+        </TR>
+      </THead>
 
-      {rdvs.length > 0 && (
-        <Table asDiv={true} caption={{ text: 'Liste de mes événements' }}>
-          <THead>
-            <TR isHeader={true}>
-              <TH>Horaires</TH>
-              <TH>Bénéficiaire</TH>
-              <TH>Type</TH>
-              <TH>Modalité</TH>
-              <TH>Créé par vous</TH>
-            </TR>
-          </THead>
-
-          <TBody>
-            {renderAgenda(
-              agendaRdvs,
-              (rdv) => (
-                <RdvRow key={rdv.id} rdv={rdv} idConseiller={idConseiller} />
-              ),
-              true
-            )}
-          </TBody>
-        </Table>
-      )}
-    </>
+      <TBody>
+        {renderAgenda(
+          agendaRdvs,
+          (rdv) => (
+            <RdvRow key={rdv.id} rdv={rdv} idConseiller={idConseiller} />
+          ),
+          true
+        )}
+      </TBody>
+    </Table>
   )
 }
