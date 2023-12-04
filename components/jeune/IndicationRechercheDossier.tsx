238009de04025f05a3146d334065956be86ad0b1
Theo Gautier <theo.gautier@octo.com>
2023-12-04 13:38:12 +0100

    retours revue

--- components/jeune/IndicationRechercheDossier.tsx
+++ components/jeune/IndicationRechercheDossier.tsx
@@ -6,28 +6,21 @@ type IndicationRechercheDossierProps = {}
 
 function IndicationRechercheDossier({}: IndicationRechercheDossierProps) {
   return (
-    <ol className='inline-flex flex-wrap justify-between max-w-2xl bg-primary_lighten mb-6 p-3 rounded-base'>
+    <ol className='inline-flex flex-wrap justify-between max-w-2xl bg-primary_lighten mb-6 p-3 rounded-base list-decimal list-inside marker:font-bold'>
       <li className='text-s-regular text-center' style={{ flex: '0 0 136px' }}>
-        <Indication1 focusable='false' aria-hidden='true' className='mb-2' />
-
-        <p>1.</p>
-        <p>Ouvrez le dossier du jeune dans i-milo</p>
+        <Indication1 focusable='false' aria-hidden='true' className='my-2' />
+        Ouvrez le dossier du jeune dans i-milo
       </li>
 
       <li className='text-s-regular text-center' style={{ flex: '0 0 136px' }}>
-        <Indication2 focusable='false' aria-hidden='true' className='mb-2' />
-
-        <p>2.</p>
-        <p>
-          Le numéro de dossier à saisir se situe dans l’adresse du navigateur
-        </p>
+        <Indication2 focusable='false' aria-hidden='true' className='my-2' />
+        Le numéro de dossier à saisir se situe dans l’adresse du navigateur
       </li>
 
       <li className='text-s-regular text-center' style={{ flex: '0 0 260px' }}>
-        <Indication3 focusable='false' aria-hidden='true' className='mb-2' />
-
-        <p>3.</p>
-        <p>Sélectionnez-le pour le copier et collez-le ici</p>
+        <Indication3 focusable='false' aria-hidden='true' className='my-2' />
+        Copiez le numéro de dossier depuis la barre d’adresse puis collez-le
+        dans le champ &quot;Numéro de dossier&quot;.
       </li>
     </ol>
   )
