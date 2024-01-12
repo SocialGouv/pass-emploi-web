c1161865bb0c3e42b5a2af0d8c6915463574f141
Theo Gautier <theo.gautier@octo.com>
2024-01-12 11:37:27 +0100

    feat(us-2211): trier actions par beneficiaire

--- components/pilotage/TableauActionsAQualifier.tsx
+++ components/pilotage/TableauActionsAQualifier.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import IconComponent, { IconName } from 'components/ui/IconComponent'
+import SortIcon from 'components/ui/SortIcon'
 import Table from 'components/ui/Table/Table'
 import { TBody } from 'components/ui/Table/TBody'
 import TD from 'components/ui/Table/TD'
@@ -16,20 +17,68 @@ interface TableauActionsConseillerProps {
 export default function TableauActionsAQualifier({
   actions,
 }: TableauActionsConseillerProps) {
+  const ALPHABETIQUE = 'ASC'
+  const INVERSE = 'DESC'
+  const [actionsTriees, setActionsTriees] = useState<ActionPilotage[]>(actions)
+  const [tri, setTri] = useState<typeof ALPHABETIQUE | typeof INVERSE | null>(
+    null
+  )
+
+  function inverserTri() {
+    const nouvelOrdre =
+      tri === null
+        ? ALPHABETIQUE
+        : tri === ALPHABETIQUE
+          ? INVERSE
+          : ALPHABETIQUE
+    setTri(nouvelOrdre)
+  }
+
+  function trierActions(actionsATrier: ActionPilotage[]) {
+    const ordre = tri === ALPHABETIQUE ? 1 : -1
+    return [...actionsATrier].sort(
+      (action1, action2) =>
+        action1.beneficiaire.nom.localeCompare(action2.beneficiaire.nom) * ordre
+    )
+  }
+
+  useEffect(() => {
+    if (tri !== null) setActionsTriees(trierActions(actions))
+  }, [tri])
+
+  useEffect(() => {
+    if (tri !== null) setActionsTriees(trierActions(actions))
+    else setActionsTriees(actions)
+  }, [actions])
+
   return (
     <>
       {actions.length > 0 && (
         <Table asDiv={true} caption={{ text: 'Liste des actions à qualifier' }}>
           <THead>
             <TR isHeader={true}>
-              <TH>Bénéficiaire</TH>
+              <TH estCliquable={true}>
+                <button
+                  className='flex border-none items-center w-full h-full p-4'
+                  onClick={inverserTri}
+                  aria-label={`Afficher la liste des bénéficiaires triée par noms de famille par ordre alphabétique ${
+                    tri === ALPHABETIQUE ? 'inversé' : ''
+                  }`}
+                  title={`Afficher la liste des bénéficiaires triée par noms de famille par ordre alphabétique ${
+                    tri === ALPHABETIQUE ? 'inversé' : ''
+                  }`}
+                >
+                  Bénéficiaire
+                  <SortIcon isDesc={tri === INVERSE} isSorted={tri !== null} />
+                </button>
+              </TH>
               <TH>Date de réalisation</TH>
               <TH>Titre de l’action</TH>
             </TR>
           </THead>
 
           <TBody>
-            {actions.map((action: ActionPilotage) => (
+            {actionsTriees.map((action: ActionPilotage) => (
               <TR
                 key={action.id}
                 href={`/mes-jeunes/${action.beneficiaire.id}/actions/${action.id}`}
