869f59bb5ad23e38c817a323b97e053a69369d1c
Theo Gautier <theo.gautier@octo.com>
2024-01-15 16:18:30 +0100

    retours revue

--- components/pilotage/TableauActionsAQualifier.tsx
+++ components/pilotage/TableauActionsAQualifier.tsx
@@ -9,6 +9,7 @@ import { TH } from 'components/ui/Table/TH'
 import { THead } from 'components/ui/Table/THead'
 import TR from 'components/ui/Table/TR'
 import { ActionPilotage } from 'interfaces/action'
+import { compareJeunesByNom } from 'interfaces/jeune'
 
 interface TableauActionsConseillerProps {
   actions: Array<ActionPilotage>
@@ -17,39 +18,31 @@ interface TableauActionsConseillerProps {
 export default function TableauActionsAQualifier({
   actions,
 }: TableauActionsConseillerProps) {
-  const ALPHABETIQUE = 'ASC'
-  const INVERSE = 'DESC'
+  const ALPHABETIQUE = 'ALPHABETIQUE'
+  const INVERSE = 'INVERSE'
   const [actionsTriees, setActionsTriees] = useState<ActionPilotage[]>(actions)
-  const [tri, setTri] = useState<typeof ALPHABETIQUE | typeof INVERSE | null>(
-    null
-  )
+  const [triBeneficiaires, setTriBeneficiaires] = useState<
+    typeof ALPHABETIQUE | typeof INVERSE | null
+  >(null)
 
-  function inverserTri() {
+  function inverserTriBeneficiaires() {
     const nouvelOrdre =
-      tri === null
-        ? ALPHABETIQUE
-        : tri === ALPHABETIQUE
-          ? INVERSE
-          : ALPHABETIQUE
-    setTri(nouvelOrdre)
+      triBeneficiaires === ALPHABETIQUE ? INVERSE : ALPHABETIQUE
+    setTriBeneficiaires(nouvelOrdre)
   }
 
   function trierActions(actionsATrier: ActionPilotage[]) {
-    const ordre = tri === ALPHABETIQUE ? 1 : -1
+    const ordre = triBeneficiaires === ALPHABETIQUE ? 1 : -1
     return [...actionsATrier].sort(
       (action1, action2) =>
-        action1.beneficiaire.nom.localeCompare(action2.beneficiaire.nom) * ordre
+        compareJeunesByNom(action1.beneficiaire, action2.beneficiaire) * ordre
     )
   }
 
   useEffect(() => {
-    if (tri !== null) setActionsTriees(trierActions(actions))
-  }, [tri])
-
-  useEffect(() => {
-    if (tri !== null) setActionsTriees(trierActions(actions))
-    else setActionsTriees(actions)
-  }, [actions])
+    if (!triBeneficiaires) setActionsTriees(actions)
+    else setActionsTriees(trierActions(actions))
+  }, [actions, triBeneficiaires])
 
   return (
     <>
@@ -60,16 +53,19 @@ export default function TableauActionsAQualifier({
               <TH estCliquable={true}>
                 <button
                   className='flex border-none items-center w-full h-full p-4'
-                  onClick={inverserTri}
+                  onClick={inverserTriBeneficiaires}
                   aria-label={`Afficher la liste des bénéficiaires triée par noms de famille par ordre alphabétique ${
-                    tri === ALPHABETIQUE ? 'inversé' : ''
+                    triBeneficiaires === ALPHABETIQUE ? 'inversé' : ''
                   }`}
                   title={`Afficher la liste des bénéficiaires triée par noms de famille par ordre alphabétique ${
-                    tri === ALPHABETIQUE ? 'inversé' : ''
+                    triBeneficiaires === ALPHABETIQUE ? 'inversé' : ''
                   }`}
                 >
                   Bénéficiaire
-                  <SortIcon isDesc={tri === INVERSE} isSorted={tri !== null} />
+                  <SortIcon
+                    isDesc={triBeneficiaires === INVERSE}
+                    isSorted={triBeneficiaires !== null}
+                  />
                 </button>
               </TH>
               <TH>Date de réalisation</TH>
