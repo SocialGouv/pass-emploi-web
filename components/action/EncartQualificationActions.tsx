83347fcc231faf053417c64f5f510c0435e04c44
Theo Gautier <theo.gautier@octo.com>
2024-01-29 11:53:45 +0100

    test(us-2256): ajoute tests

--- components/action/EncartQualificationActions.tsx
+++ components/action/EncartQualificationActions.tsx
@@ -1,47 +1,100 @@
+import { useState } from 'react'
+
+import ConfirmationMultiQualificationModal from 'components/ConfirmationMultiQualificationModal'
+import ConfirmationMultiQualificationModalNonSNP from 'components/ConfirmationMultiQualificationModalNonSNP'
 import Button, { ButtonStyle } from 'components/ui/Button/Button'
+import { ActionAQualifier } from 'interfaces/action'
+import { BaseJeune } from 'interfaces/jeune'
 
 type EncartQualificationActionsProps = {
+  actionsSelectionnees: ActionAQualifier[]
   boutonsDisabled: boolean
+  jeune?: BaseJeune
   nombreActionsSelectionnees: number
-  onQualificationSNP: (afficherModale: boolean) => void
-  onQualificationNonSNP: (afficherModale: boolean) => void
+  onLienExterne: (label: string) => void
+  onQualification: (
+    qualificationSNP: boolean,
+    actionsSelectionnees: Array<{ idAction: string; codeQualification: string }>
+  ) => Promise<void>
 }
 
 export default function EncartQualificationActions({
+  actionsSelectionnees,
   boutonsDisabled,
+  jeune,
   nombreActionsSelectionnees,
-  onQualificationSNP,
-  onQualificationNonSNP,
+  onLienExterne,
+  onQualification,
 }: EncartQualificationActionsProps) {
-  return (
-    <div className='flex items-center bg-primary_lighten rounded-base p-4 justify-between'>
-      <p className='whitespace-pre-wrap'>
-        {nombreActionsSelectionnees === 0 &&
-          'Sélectionnez au moins un élément ci-dessous \npour commencer à qualifier'}
-        {nombreActionsSelectionnees === 1 &&
-          '1 action sélectionnée. \nS’agit-il de SNP ou de non SNP ?'}
-        {nombreActionsSelectionnees > 1 &&
-          `${nombreActionsSelectionnees} actions sélectionnées. \nS’agit-il de SNP ou de non SNP ?`}
-      </p>
+  const [
+    afficherModaleMultiQualification,
+    setAfficherModaleMultiQualification,
+  ] = useState<boolean>(false)
+  const [
+    afficherModaleMultiQualificationNonSNP,
+    setAfficherModaleMultiQualificationNonSNP,
+  ] = useState<boolean>(false)
 
-      <div className='flex gap-2'>
-        <Button
-          onClick={() => onQualificationNonSNP(true)}
-          style={ButtonStyle.SECONDARY}
-          label='Enregistrer les actions en non SNP'
-          disabled={boutonsDisabled}
-        >
-          Enregistrer les actions en non SNP
-        </Button>
-        <Button
-          onClick={() => onQualificationSNP(true)}
-          style={ButtonStyle.PRIMARY}
-          label='Qualifier les actions en SNP'
-          disabled={boutonsDisabled}
-        >
-          Qualifier les actions en SNP
-        </Button>
+  async function qualifier(enSNP: boolean) {
+    await onQualification(
+      enSNP,
+      actionsSelectionnees as Array<{
+        idAction: string
+        codeQualification: string
+      }>
+    )
+  }
+
+  return (
+    <>
+      <div className='flex items-center bg-primary_lighten rounded-base p-4 justify-between'>
+        <p className='whitespace-pre-wrap'>
+          {nombreActionsSelectionnees === 0 &&
+            'Sélectionnez au moins un élément ci-dessous \npour commencer à qualifier'}
+          {nombreActionsSelectionnees === 1 &&
+            '1 action sélectionnée. \nS’agit-il de SNP ou de non SNP ?'}
+          {nombreActionsSelectionnees > 1 &&
+            `${nombreActionsSelectionnees} actions sélectionnées. \nS’agit-il de SNP ou de non SNP ?`}
+        </p>
+
+        <div className='flex gap-2'>
+          <Button
+            onClick={() => setAfficherModaleMultiQualificationNonSNP(true)}
+            style={ButtonStyle.SECONDARY}
+            label='Enregistrer les actions en non SNP'
+            disabled={boutonsDisabled}
+          >
+            Enregistrer les actions en non SNP
+          </Button>
+          <Button
+            onClick={() => setAfficherModaleMultiQualification(true)}
+            style={ButtonStyle.PRIMARY}
+            label='Qualifier les actions en SNP'
+            disabled={boutonsDisabled}
+          >
+            Qualifier les actions en SNP
+          </Button>
+        </div>
       </div>
-    </div>
+
+      {afficherModaleMultiQualification && (
+        <ConfirmationMultiQualificationModal
+          actions={actionsSelectionnees}
+          beneficiaire={jeune!}
+          onConfirmation={() => qualifier(true)}
+          onCancel={() => setAfficherModaleMultiQualification(false)}
+          onLienExterne={onLienExterne}
+        />
+      )}
+
+      {afficherModaleMultiQualificationNonSNP && (
+        <ConfirmationMultiQualificationModalNonSNP
+          actions={actionsSelectionnees}
+          beneficiaire={jeune!}
+          onConfirmation={() => qualifier(false)}
+          onCancel={() => setAfficherModaleMultiQualificationNonSNP(false)}
+        />
+      )}
+    </>
   )
 }
