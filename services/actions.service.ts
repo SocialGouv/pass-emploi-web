d62ad273cac863fe7f44987652de7308f844c147
anhara.akter <anhara.akter@octo.com>
2024-01-30 15:06:51 +0100

    feat(us-2253): fix retour

--- services/actions.service.ts
+++ services/actions.service.ts
@@ -359,7 +359,7 @@ function extraireNombreActionsAvecStatut(
   statut: StatutAction
 ): number {
   switch (statut) {
-    case StatutAction.AFaire:
+    case StatutAction.Afaire:
       return metadonnees.nombrePasCommencees + metadonnees.nombreEnCours
     case StatutAction.Terminee:
       return metadonnees.nombreAQualifier
