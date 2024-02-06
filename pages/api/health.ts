833bd51708e0e1a6a6d8d26d3c76a291d429933c
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2024-02-06 17:56:37 +0100

    feat: move api /health to /app

--- pages/api/health.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default async function health(
-  _req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.status(200).json({ status: 'ok' })
-}
