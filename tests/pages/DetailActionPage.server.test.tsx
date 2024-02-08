f5b6c27890fab60233545e547399ce145c191a02
Theo Gautier <theo.gautier@octo.com>
2024-02-08 15:01:02 +0100

    feat(us-2316): return to pilotage qualification action

--- tests/pages/DetailActionPage.server.test.tsx
+++ tests/pages/DetailActionPage.server.test.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react'
+import { headers } from 'next/headers'
 import { notFound } from 'next/navigation'
 
 import DetailActionPage from 'app/(connected)/(with-sidebar)/(with-chat)/mes-jeunes/[idJeune]/actions/[idAction]/DetailActionPage'
@@ -20,6 +21,7 @@ jest.mock('services/jeunes.service')
 jest.mock(
   'app/(connected)/(with-sidebar)/(with-chat)/mes-jeunes/[idJeune]/actions/[idAction]/DetailActionPage'
 )
+jest.mock('next/headers', () => ({ headers: jest.fn(() => new Map()) }))
 
 describe('ActionPage server side', () => {
   describe('quand le conseiller est Pôle emploi', () => {
@@ -46,6 +48,9 @@ describe('ActionPage server side', () => {
         accessToken: 'accessToken',
         user: { structure: 'MILO', id: 'id-conseiller' },
       })
+      ;(headers as jest.Mock).mockReturnValue(
+        new Map([['referer', '/whatever']])
+      )
     })
 
     it("récupère les info de l'action et du jeune", async () => {
@@ -77,9 +82,50 @@ describe('ActionPage server side', () => {
           jeune,
           commentaires,
           lectureSeule: false,
+          from: 'beneficiaire',
         },
         {}
       )
+    })
+
+    describe('quand le conseiller vient de pilotage', () => {
+      it('prépare la page', async () => {
+        const action = uneAction()
+        const commentaires = [unCommentaire()]
+        const jeune: BaseJeune & { idConseiller: string } = {
+          id: 'jeune-1',
+          prenom: 'Nadia',
+          nom: 'Sanfamiye',
+          idConseiller: 'id-conseiller',
+        }
+        ;(getAction as jest.Mock).mockResolvedValue({ action, jeune })
+        ;(recupererLesCommentaires as jest.Mock).mockResolvedValue(commentaires)
+        ;(getJeuneDetails as jest.Mock).mockResolvedValue(unDetailJeune())
+        ;(headers as jest.Mock).mockReturnValue(
+          new Map([['referer', '/pilotage']])
+        )
+
+        // When
+        const params = { idJeune: 'jeune-1', idAction: 'id-action' }
+        const metadata = await generateMetadata({ params })
+        render(await DetailAction({ params }))
+
+        // Then
+        expect(getAction).toHaveBeenCalledWith('id-action', 'accessToken')
+        expect(metadata).toEqual({
+          title: `${action.content} - Actions de ${jeune.prenom} ${jeune.nom} - Portefeuille`,
+        })
+        expect(DetailActionPage).toHaveBeenCalledWith(
+          {
+            action,
+            jeune,
+            commentaires,
+            lectureSeule: false,
+            from: 'pilotage',
+          },
+          {}
+        )
+      })
     })
 
     describe("quand l'action n'existe pas", () => {
