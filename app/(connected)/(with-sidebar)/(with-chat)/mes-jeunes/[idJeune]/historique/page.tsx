693a3c5b81d01e901cf98670a270baa17a574c08
anhara.akter <anhara.akter@octo.com>
2024-02-16 11:22:58 +0100

    feat(us-2312): Modifier titre de Historique

--- app/(connected)/(with-sidebar)/(with-chat)/mes-jeunes/[idJeune]/historique/page.tsx
+++ app/(connected)/(with-sidebar)/(with-chat)/mes-jeunes/[idJeune]/historique/page.tsx
@@ -49,7 +49,7 @@ export default async function Historique({
   return (
     <>
       <PageFilArianePortal />
-      <PageHeaderPortal header='Historique' />
+      <PageHeaderPortal header={`${beneficiaire.prenom} ${beneficiaire.nom}`} />
 
       <HistoriquePage
         conseillers={conseillers}
