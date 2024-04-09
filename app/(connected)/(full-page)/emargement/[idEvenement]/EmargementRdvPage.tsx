7d28ed8db62711ee805807558e07dc236c71bf3e
Theo Gautier <theo.gautier@octo.com>
2024-04-09 11:21:31 +0200

    feat(us-2499): plus dinfos sur la feuille d emargement

--- app/(connected)/(full-page)/emargement/[idEvenement]/EmargementRdvPage.tsx
+++ app/(connected)/(full-page)/emargement/[idEvenement]/EmargementRdvPage.tsx
@@ -11,9 +11,36 @@ import { TH } from 'components/ui/Table/TH'
 import { THead } from 'components/ui/Table/THead'
 import TR from 'components/ui/Table/TR'
 import { Evenement } from 'interfaces/evenement'
+import { BaseJeune, compareJeunesByNom } from 'interfaces/jeune'
+import { Session, StatutBeneficiaire } from 'interfaces/session'
 import { toFrenchDateTime } from 'utils/date'
 
-function EmargementRdvPage({ evenement }: { evenement: Evenement }) {
+type EmargementRdvPageProps = {
+  evenement: Evenement | Session
+  agence?: string
+}
+
+function EmargementRdvPage({ evenement, agence }: EmargementRdvPageProps) {
+  const isSession = assertIsSession(evenement)
+  const inscriptions: Array<BaseJeune> = isSession
+    ? evenement.inscriptions
+        .filter(
+          (beneficiaire) => beneficiaire.statut === StatutBeneficiaire.INSCRIT
+        )
+        .map((b) => ({
+          id: b.idJeune,
+          nom: b.nom,
+          prenom: b.prenom,
+        }))
+        .sort(compareJeunesByNom)
+    : evenement.jeunes
+        .map((b) => ({
+          id: b.id,
+          nom: b.nom,
+          prenom: b.prenom,
+        }))
+        .sort(compareJeunesByNom)
+
   useEffect(() => {
     window.print()
   }, [])
@@ -23,7 +50,7 @@ function EmargementRdvPage({ evenement }: { evenement: Evenement }) {
       <header className='bg-primary text-blanc flex justify-between py-12 px-8 mb-8 print:page-break-before'>
         <div>
           <h1 className='text-l-bold'>Feuille d’émargement</h1>
-          <h2>{evenement.titre}</h2>
+          <h2>Mission Locale de {agence}</h2>
         </div>
         <LogoCEJ
           role='img'
@@ -34,43 +61,113 @@ function EmargementRdvPage({ evenement }: { evenement: Evenement }) {
         />
       </header>
       <div className='mb-8'>
-        <ul>
-          <li>
-            Atelier
-            {' : '}
-            <span className='text-base-bold'>{evenement.titre}</span>
-          </li>
-          <li>
-            Créateur{' : '}
-            <span className='text-base-bold'>
-              {evenement.createur.prenom} {evenement.createur.nom}
-            </span>
-          </li>
-          <li>
-            Date et heure{' : '}
-            <span className='text-base-bold'>
-              {toFrenchDateTime(evenement.date)}
-            </span>
-          </li>
-          <li>
-            Adresse{' : '}
-            <span className='text-base-bold'>
-              {evenement.adresse ?? (
-                <>
-                  --
-                  <span className='sr-only'>information non disponible</span>
-                </>
-              )}
-            </span>
-          </li>
-        </ul>
+        <dl>
+          {isSession && (
+            <>
+              <div className='mb-2'>
+                <dt className='inline'>
+                  <span>Type : </span>
+                </dt>
+                <dd className='text-base-bold inline'>
+                  {evenement.offre.type}
+                </dd>
+              </div>
+
+              <div className='mb-2'>
+                <dt className='inline'>
+                  <span>Titre : </span>
+                </dt>
+                <dd className='text-base-bold inline'>
+                  {evenement.offre.titre} - {evenement.session.nom}
+                </dd>
+              </div>
+
+              <div className='mb-2'>
+                <dt className='inline'>
+                  <span>Animateur : </span>
+                </dt>
+                <dd className='text-base-bold inline'>
+                  {evenement.session.animateur ?? (
+                    <>
+                      --
+                      <span className='sr-only'>
+                        information non disponible
+                      </span>
+                    </>
+                  )}
+                </dd>
+              </div>
+
+              <div className='mb-2'>
+                <dt className='inline'>
+                  <span>Date et heure : </span>
+                </dt>
+                <dd className='text-base-bold inline'>
+                  {toFrenchDateTime(evenement.session.dateHeureDebut)}
+                </dd>
+              </div>
+            </>
+          )}
+
+          {!isSession && (
+            <>
+              <div className='mb-2'>
+                <dt className='inline'>
+                  <span>Atelier : </span>
+                </dt>
+                <dd className='text-base-bold inline'>{evenement.titre}</dd>
+              </div>
+
+              <div className='mb-2'>
+                <dt className='inline'>
+                  <span>Organisme : </span>
+                </dt>
+                <dd className='text-base-bold inline'>
+                  {evenement.organisme ?? (
+                    <>
+                      --
+                      <span className='sr-only'>
+                        information non disponible
+                      </span>
+                    </>
+                  )}
+                </dd>
+              </div>
+
+              <div className='mb-2'>
+                <dt className='inline'>
+                  <span>Date et heure : </span>
+                </dt>
+                <dd className='text-base-bold inline'>
+                  {toFrenchDateTime(evenement.date)}
+                </dd>
+              </div>
+
+              <div className='mb-2'>
+                <dt className='inline'>
+                  <span>Adresse : </span>
+                </dt>
+                <dd className='text-base-bold inline'>
+                  {evenement.adresse ?? (
+                    <>
+                      --
+                      <span className='sr-only'>
+                        information non disponible
+                      </span>
+                    </>
+                  )}
+                </dd>
+              </div>
+            </>
+          )}
+        </dl>
       </div>
 
       <Table
         asDiv={true}
         caption={{
           text: `Nombre de bénéficiaires inscrits`,
-          count: evenement.jeunes.length,
+          count: inscriptions.length,
           visible: true,
         }}
       >
@@ -82,19 +179,23 @@ function EmargementRdvPage({ evenement }: { evenement: Evenement }) {
           </TR>
         </THead>
         <TBody>
-          {evenement.jeunes.map((jeune, key) => (
+          {inscriptions.map((jeune, key) => (
             <TR key={key}>
               <TD>{key + 1}</TD>
               <TD>
                 {jeune.prenom} {jeune.nom}
               </TD>
-              <TD className='py-12 border-l border-grey_500'></TD>
+              <TD className='py-8 border-l border-grey_500'></TD>
             </TR>
           ))}
         </TBody>
       </Table>
     </>
   )
+}
+
+function assertIsSession(evenement: Evenement | Session): evenement is Session {
+  return Object.prototype.hasOwnProperty.call(evenement, 'inscriptions')
 }
 
 export default withTransaction(
--- tests/pages/EmargementRdvPage.server.test.tsx
+++ tests/pages/EmargementRdvPage.server.test.tsx
@@ -5,9 +5,13 @@ import EmargementRdvPage from 'app/(connected)/(full-page)/emargement/[idEveneme
 import EmargementRdv, {
   generateMetadata,
 } from 'app/(connected)/(full-page)/emargement/[idEvenement]/page'
+import { unConseiller } from 'fixtures/conseiller'
 import { unEvenement } from 'fixtures/evenement'
+import { unDetailSession } from 'fixtures/session'
 import { StructureConseiller } from 'interfaces/conseiller'
+import { getConseillerServerSide } from 'services/conseiller.service'
 import { getDetailsEvenement } from 'services/evenements.service'
+import { getDetailsSession } from 'services/sessions.service'
 import { getMandatorySessionServerSide } from 'utils/auth/auth'
 
 jest.mock('utils/auth/auth', () => ({
@@ -16,42 +20,91 @@ jest.mock('utils/auth/auth', () => ({
 jest.mock(
   'app/(connected)/(full-page)/emargement/[idEvenement]/EmargementRdvPage'
 )
+jest.mock('services/conseiller.service')
+jest.mock('services/sessions.service')
 jest.mock('services/evenements.service')
 
 describe('EmargementRdvPage server side', () => {
-  const evenementAEmarger = unEvenement({
+  const acAEmarger = unEvenement({
     titre: 'Meeting de la famille Pirate',
   })
+
+  const sessionAEmarger = unDetailSession()
+
+  const conseiller = unConseiller({
+    structure: StructureConseiller.MILO,
+    structureMilo: { id: 'id-agence', nom: 'Montastruc-la-Conseillère' },
+  })
+
   beforeEach(() => {
     // Given
     ;(getMandatorySessionServerSide as jest.Mock).mockResolvedValue({
-      user: { id: 'id-conseiller' },
+      user: { id: 'id-conseiller', structure: StructureConseiller.MILO },
       accessToken: 'accessToken',
     })
-    ;(getDetailsEvenement as jest.Mock).mockResolvedValue(evenementAEmarger)
+    ;(getDetailsEvenement as jest.Mock).mockResolvedValue(acAEmarger)
+    ;(getDetailsSession as jest.Mock).mockResolvedValue(sessionAEmarger)
+    ;(getConseillerServerSide as jest.Mock).mockResolvedValue(conseiller)
   })
 
-  it('prépare la page', async () => {
-    // When
-    const metadata = await generateMetadata({
-      params: { idEvenement: 'id-evenement' },
-    })
-    render(
-      await EmargementRdv({
+  describe('Quand l’événement est une ac', () => {
+    it('prépare la page', async () => {
+      // When
+      const metadata = await generateMetadata({
         params: { idEvenement: 'id-evenement' },
+        searchParams: { type: 'ac' },
       })
-    )
 
-    // Then
-    expect(metadata).toEqual({
-      title: 'Emargement - Meeting de la famille Pirate',
+      render(
+        await EmargementRdv({
+          params: { idEvenement: 'id-evenement' },
+          searchParams: { type: 'ac' },
+        })
+      )
+
+      // Then
+      expect(metadata).toEqual({
+        title: 'Emargement - Meeting de la famille Pirate',
+      })
+
+      expect(EmargementRdvPage).toHaveBeenCalledWith(
+        {
+          evenement: acAEmarger,
+          agence: 'Montastruc-la-Conseillère',
+        },
+        {}
+      )
     })
-    expect(EmargementRdvPage).toHaveBeenCalledWith(
-      {
-        evenement: evenementAEmarger,
-      },
-      {}
-    )
+  })
+
+  describe('Quand l’événement est une session', () => {
+    it('prépare la page', async () => {
+      // When
+      const metadata = await generateMetadata({
+        params: { idEvenement: 'id-evenement' },
+        searchParams: { type: 'session' },
+      })
+
+      render(
+        await EmargementRdv({
+          params: { idEvenement: 'id-evenement' },
+          searchParams: { type: 'session' },
+        })
+      )
+
+      // Then
+      expect(metadata).toEqual({
+        title: `Emargement - ${sessionAEmarger.session.nom}`,
+      })
+
+      expect(EmargementRdvPage).toHaveBeenCalledWith(
+        {
+          evenement: sessionAEmarger,
+          agence: 'Montastruc-la-Conseillère',
+        },
+        {}
+      )
+    })
   })
 
   describe('Quand le conseiller est France Travail', () => {
@@ -66,7 +119,10 @@ describe('EmargementRdvPage server side', () => {
       })
 
       // When
-      const promise = EmargementRdv({ params: { idEvenement: 'id-evenement' } })
+      const promise = EmargementRdv({
+        params: { idEvenement: 'id-evenement' },
+        searchParams: { type: 'ac' },
+      })
 
       //Then
       await expect(promise).rejects.toEqual(
