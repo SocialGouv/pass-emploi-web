9d92a4d41a376695ede6079f91a891c262c7774e
Theo Gautier <theo.gautier@octo.com>
2024-04-29 16:35:51 +0200

    fix: visibilite session

--- components/ui/SpinningLoader.tsx
+++ components/ui/SpinningLoader.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 import IconComponent, { IconName } from 'components/ui/IconComponent'
 
-export function SpinningLoader() {
+export function SpinningLoader({ className }: { className?: string }) {
   return (
     <IconComponent
       name={IconName.Spinner}
       aria-label='Chargement…'
       focusable={false}
-      className='w-20 h-20 m-auto fill-primary animate-spin'
+      className={`w-20 h-20 m-auto fill-primary animate-spin ${className ?? ''}`}
       title='Chargement…'
     />
   )
