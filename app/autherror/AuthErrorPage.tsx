a28a36a0d896798a80822f2310a643f49f316b27
Malek Zemni <malek.zemni@octo.com>
2024-06-05 17:59:47 +0200

    feat: améliorer CSS page autherror

--- app/autherror/AuthErrorPage.tsx
+++ app/autherror/AuthErrorPage.tsx
@@ -1,17 +1,16 @@
 'use client'
 
 import { withTransaction } from '@elastic/apm-rum-react'
-import React from 'react'
 
-import { IconName } from 'components/ui/IconComponent'
-import ExternalLink from 'components/ui/Navigation/ExternalLink'
+import { TypeUtilisateur } from './page'
+
 import { StructureConseiller } from 'interfaces/conseiller'
 import { trackEvent, trackEventBeneficiaire } from 'utils/analytics/matomo'
 
 type AuthErrorPageProps = {
   erreur: string
   utilisateur: {
-    type?: 'beneficiaire' | 'conseiller'
+    type?: TypeUtilisateur
     structure?: StructureConseiller
   }
 }
@@ -19,7 +18,7 @@ function AuthErrorPage({ erreur, utilisateur }: AuthErrorPageProps) {
   function trackContacterSupportClick() {
     if (!utilisateur.type || !utilisateur.structure) return
 
-    if (utilisateur.type === 'conseiller')
+    if (utilisateur.type === TypeUtilisateur.CONSEILLER)
       trackEvent({
         structure: utilisateur.structure,
         categorie: 'Contact Support',
@@ -28,7 +27,7 @@ function AuthErrorPage({ erreur, utilisateur }: AuthErrorPageProps) {
         aDesBeneficiaires: null,
       })
 
-    if (utilisateur.type === 'beneficiaire')
+    if (utilisateur.type === TypeUtilisateur.JEUNE)
       trackEventBeneficiaire({
         structure: utilisateur.structure,
         categorie: 'Contact Support',
@@ -38,24 +37,27 @@ function AuthErrorPage({ erreur, utilisateur }: AuthErrorPageProps) {
   }
 
   return (
-    <div className='shadow-m'>
+    <>
       <header>
-        <h1 className='text-m-bold text-primary text-center mt-[48px] mb-[24px]'>
-          Pass Emploi Connect
-        </h1>
+        <title>Portail de connexion</title>
       </header>
 
-      <main>
-        <p>{erreur}</p>
+      <main className='flex flex-col justify-center p-10 mt-32 w-screen'>
+        <div className='shadow-m flex flex-col justify-center w-9/10 mx-auto p-8'>
+          <h1 className='text-m-bold text-primary text-center mt-6 mb-8'>
+            Portail de connexion
+          </h1>
+          <p className='text-center text-s'>{erreur}</p>
 
-        <ExternalLink
-          href={'mailto:' + process.env.NEXT_PUBLIC_SUPPORT_MAIL}
-          label={'contacter le support'}
-          iconName={IconName.OutgoingMail}
-          onClick={trackContacterSupportClick}
-        />
+          {/* <ExternalLink
+            href={'mailto:' + process.env.NEXT_PUBLIC_SUPPORT_MAIL}
+            label={'contacter le support'}
+            iconName={IconName.OutgoingMail}
+            onClick={trackContacterSupportClick}
+          /> */}
+        </div>
       </main>
-    </div>
+    </>
   )
 }
 
--- app/autherror/page.tsx
+++ app/autherror/page.tsx
@@ -1,10 +1,15 @@
 import AuthErrorPage from 'app/autherror/AuthErrorPage'
 import { StructureConseiller } from 'interfaces/conseiller'
 
+export enum TypeUtilisateur {
+  CONSEILLER = 'CONSEILLER',
+  JEUNE = 'JEUNE',
+}
+
 type AuthErrorSearchParams = Partial<{
   reason: string
-  utilisateur: 'beneficiaire' | 'conseiller'
-  structure: StructureConseiller
+  typeUtilisateur: TypeUtilisateur
+  structureUtilisateur: StructureConseiller
 }>
 
 export default function AuthError({
@@ -15,17 +20,53 @@ export default function AuthError({
   let erreur: string
   switch (searchParams?.reason) {
     case 'UTILISATEUR_INEXISTANT':
-      erreur = 'Aucun utilisateur trouvé, veuillez contacter votre conseiller'
+      erreur =
+        "Votre compte n'est pas enregistré sur l'application, veuillez contacter votre conseiller"
+      break
+    case 'UTILISATEUR_DEJA_MILO':
+      erreur =
+        'Veuillez vous connecter en choisissant Mission Locale ou contacter votre conseiller pour recréer le compte'
+      break
+    case 'UTILISATEUR_NOUVEAU_MILO':
+      erreur =
+        'Veuillez vous connecter en choisissant Mission Locale ou contacter votre conseiller pour recréer le compte'
+      break
+    case 'UTILISATEUR_DEJA_PE':
+      erreur =
+        "Veuillez vous connecter en choisissant France Travail sur l'application CEJ ou contacter votre conseiller pour recréer le compte"
+      break
+    case 'UTILISATEUR_NOUVEAU_PE':
+      erreur =
+        "Veuillez vous connecter en choisissant France Travail sur l'application CEJ ou contacter votre conseiller pour recréer le compte"
+      break
+    case 'UTILISATEUR_DEJA_PE_BRSA':
+      erreur =
+        "Veuillez vous connecter en choisissant France Travail sur l'application Pass Emploi ou contacter votre conseiller pour recréer le compte"
+      break
+    case 'UTILISATEUR_NOUVEAU_PE_BRSA':
+      erreur =
+        "Veuillez vous connecter en choisissant France Travail sur l'application Pass Emploi ou contacter votre conseiller pour recréer le compte"
       break
     default:
-      erreur = 'Une erreur inconnue est survenue'
+      if (
+        searchParams?.typeUtilisateur === TypeUtilisateur.CONSEILLER &&
+        (searchParams?.structureUtilisateur ===
+          StructureConseiller.POLE_EMPLOI ||
+          searchParams?.structureUtilisateur ===
+            StructureConseiller.POLE_EMPLOI_BRSA)
+      ) {
+        erreur =
+          'Veuillez vous connecter en choisissant France Travail CEJ / BRSA ou contacter le support'
+      } else {
+        erreur = 'Une erreur inconnue est survenue, veuillez réessayer'
+      }
       if (searchParams?.reason) erreur += ` (code: ${searchParams.reason})`
       break
   }
 
   const utilisateur = {
-    type: searchParams?.utilisateur,
-    structure: searchParams?.structure,
+    type: searchParams?.typeUtilisateur,
+    structure: searchParams?.structureUtilisateur,
   }
 
   return <AuthErrorPage erreur={erreur} utilisateur={utilisateur} />
