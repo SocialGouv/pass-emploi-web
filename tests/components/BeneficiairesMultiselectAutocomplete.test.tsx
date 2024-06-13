1f7d23d3481d9d698cebafe43afd8b2127437493
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2024-06-13 17:53:11 +0200

    a11y: add jest-axe

--- tests/components/BeneficiairesMultiselectAutocomplete.test.tsx
+++ tests/components/BeneficiairesMultiselectAutocomplete.test.tsx
@@ -1,6 +1,7 @@
 import { screen, within } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { axe } from 'jest-axe'
+import { axe, toHaveNoViolations } from 'jest-axe'
+expect.extend(toHaveNoViolations)
 
 import BeneficiairesMultiselectAutocomplete, {
   OptionBeneficiaire,
