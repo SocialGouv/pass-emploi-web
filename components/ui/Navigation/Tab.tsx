10c317507da7d366cb6febc705c80f5ce51e3fbf
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2024-07-15 17:35:35 +0200

    fix: fix tablist/tab

--- components/ui/Navigation/Tab.tsx
+++ components/ui/Navigation/Tab.tsx
@@ -20,40 +20,44 @@ export default function Tab({
   iconName,
 }: TabProps) {
   return (
-    <li
-      role='tab'
-      id={`${controls}--tab`}
-      tabIndex={selected ? 0 : -1}
-      aria-controls={controls}
-      aria-selected={selected}
-      onClick={onSelectTab}
-      className={`text-m-medium text-grey_800 px-4 pb-2 flex items-center cursor-pointer ${
-        selected ? 'text-m-bold border-b-4 border-b-primary' : 'hover:font-bold'
-      }`}
-    >
-      {iconName && (
-        <IconComponent
-          name={iconName}
-          aria-hidden={true}
-          focusable={false}
-          className={`w-4 h-4 mr-2 ${
-            selected
-              ? 'fill-primary stroke-primary'
-              : 'fill-grey_800 stroke-grey_800'
-          }`}
-        />
-      )}
-      {label}
-      {count !== undefined && (
-        <span className='ml-4'>
-          <Badge
-            count={count}
-            textColor='primary'
-            bgColor='primary_lighten'
-            size={6}
+    <li role='none'>
+      <button
+        role='tab'
+        tabIndex={selected ? 0 : -1}
+        id={`${controls}--tab`}
+        aria-controls={controls}
+        aria-selected={selected}
+        onClick={onSelectTab}
+        className={`text-m-medium text-grey_800 px-4 pb-2 flex items-center cursor-pointer ${
+          selected
+            ? 'text-m-bold border-b-4 border-b-primary'
+            : 'hover:font-bold'
+        }`}
+      >
+        {iconName && (
+          <IconComponent
+            name={iconName}
+            aria-hidden={true}
+            focusable={false}
+            className={`w-4 h-4 mr-2 ${
+              selected
+                ? 'fill-primary stroke-primary'
+                : 'fill-grey_800 stroke-grey_800'
+            }`}
           />
-        </span>
-      )}
+        )}
+        {label}
+        {count !== undefined && (
+          <span className='ml-4'>
+            <Badge
+              count={count}
+              textColor='primary'
+              bgColor='primary_lighten'
+              size={6}
+            />
+          </span>
+        )}
+      </button>
     </li>
   )
 }
