1f53b3e85674ab423c8faca15ea710fe8b6bac18
Malek Zemni <malek.zemni@octo.com>
2024-07-15 14:50:41 +0200

    refactor: simplifier gestion auth error avec codes
    (cherry picked from commit 28aee787e0e373d3fc1d7747680761b0f4b8327a)

--- app/autherror/page.tsx
+++ app/autherror/page.tsx
@@ -2,7 +2,6 @@ import AuthErrorPage from 'app/autherror/AuthErrorPage'
 
 type AuthErrorSearchParams = Partial<{
   reason?: string
-  message?: string
 }>
 
 export default function AuthError({
@@ -11,57 +10,33 @@ export default function AuthError({
   searchParams?: AuthErrorSearchParams
 }) {
   let erreur: string
-  if (searchParams?.message) {
-    erreur = searchParams?.message
-  } else {
-    // legacy après MEP du back et de pass-emploi-connect "bouton unique AIJ"
-    switch (searchParams?.reason) {
-      case 'UTILISATEUR_INEXISTANT':
-        erreur =
-          "Votre compte n'est pas enregistré sur l'application, veuillez contacter votre conseiller."
-        break
-      case 'UTILISATEUR_DEJA_MILO':
-        erreur =
-          'Veuillez vous connecter en choisissant Mission Locale ou contacter votre conseiller pour recréer le compte.'
-        break
-      case 'UTILISATEUR_NOUVEAU_MILO':
-        erreur =
-          'Veuillez vous connecter en choisissant Mission Locale ou contacter votre conseiller pour recréer le compte.'
-        break
-      case 'UTILISATEUR_DEJA_PE':
-        erreur =
-          "Veuillez vous connecter en choisissant France Travail sur l'application CEJ ou contacter votre conseiller pour recréer le compte."
-        break
-      case 'UTILISATEUR_NOUVEAU_PE':
-        erreur =
-          "Veuillez vous connecter en choisissant France Travail sur l'application CEJ ou contacter votre conseiller pour recréer le compte."
-        break
-      case 'UTILISATEUR_DEJA_PE_BRSA':
-        erreur =
-          "Veuillez vous connecter en choisissant France Travail BRSA sur l'application Pass Emploi ou contacter votre conseiller pour recréer le compte."
-        break
-      case 'UTILISATEUR_NOUVEAU_PE_BRSA':
-        erreur =
-          "Veuillez vous connecter en choisissant France Travail BRSA sur l'application Pass Emploi ou contacter votre conseiller pour recréer le compte."
-        break
-      case 'UTILISATEUR_DEJA_PE_AIJ':
-        erreur =
-          "Veuillez vous connecter en choisissant France Travail AIJ sur l'application Pass Emploi ou contacter votre conseiller pour recréer le compte."
-        break
-      case 'UTILISATEUR_NOUVEAU_PE_AIJ':
-        erreur =
-          "Veuillez vous connecter en choisissant France Travail AIJ sur l'application Pass Emploi ou contacter votre conseiller pour recréer le compte."
-        break
-      case 'UTILISATEUR_CONSEILLER_MAUVAISE_STRUCTURE':
-        erreur =
-          "Veuillez vous connecter en choisissant le bon accompagnement (CEJ / BRSA / AIJ). Si vous avez changé d'accompagnement, veuillez supprimer votre compte ou contacter le support."
-        break
-      default:
-        erreur =
-          'Une erreur inconnue est survenue, veuillez réessayer ou contacter le support.'
-        if (searchParams?.reason) erreur += ` (code: ${searchParams.reason})`
-        break
-    }
+  switch (searchParams?.reason) {
+    case 'UTILISATEUR_INEXISTANT':
+      erreur =
+        "Votre compte n'est pas enregistré sur l'application, veuillez contacter votre conseiller."
+      break
+    case 'UTILISATEUR_DEJA_MILO':
+      erreur =
+        "Veuillez vous connecter en choisissant Mission Locale sur l'application du CEJ ou contacter votre conseiller pour recréer le compte."
+      break
+    case 'UTILISATEUR_DEJA_PE':
+      erreur =
+        "Veuillez vous connecter en choisissant France Travail sur l'application CEJ ou contacter votre conseiller pour recréer le compte."
+      break
+    case 'UTILISATEUR_DEJA_PE_BRSA':
+    case 'UTILISATEUR_DEJA_PE_AIJ':
+      erreur =
+        "Veuillez vous connecter en choisissant France Travail sur l'application Pass Emploi ou contacter votre conseiller pour recréer le compte."
+      break
+    case 'UTILISATEUR_CONSEILLER_MAUVAISE_STRUCTURE':
+      erreur =
+        "Veuillez vous connecter en choisissant le bon accompagnement (CEJ / BRSA / AIJ). Si vous avez changé d'accompagnement, veuillez supprimer votre compte ou contacter le support."
+      break
+    default:
+      erreur =
+        'Une erreur inconnue est survenue, veuillez réessayer ou contacter le support.'
+      if (searchParams?.reason) erreur += ` (code: ${searchParams.reason})`
+      break
   }
 
   return <AuthErrorPage erreur={erreur} />
