cabecfcc580f057492385883ea1c82e444122dc4
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2024-07-16 17:39:48 +0200

    fix: corrige IconToggle

---
+++ components/ui/Form/IconToggle.tsx
@@ -0,0 +1,46 @@
+import IconComponent, { IconName } from 'components/ui/IconComponent'
+
+type IconToggleProps = {
+  id: string
+  checked: boolean
+  checkedIconName: IconName
+  uncheckedIconName: IconName
+  actionLabel: string
+  oppositeActionLabel: string
+  onToggle: () => void
+  className?: string
+}
+
+export default function IconToggle({
+  id,
+  checked,
+  checkedIconName,
+  uncheckedIconName,
+  actionLabel,
+  oppositeActionLabel,
+  onToggle,
+  className,
+}: IconToggleProps) {
+  return (
+    <>
+      <label htmlFor={id} className='sr-only'>
+        {actionLabel}
+      </label>
+      <button
+        id={id}
+        type='button'
+        role='switch'
+        aria-checked={checked}
+        onClick={onToggle}
+        title={checked ? oppositeActionLabel : actionLabel}
+        className={className}
+      >
+        <IconComponent
+          name={checked ? checkedIconName : uncheckedIconName}
+          focusable={false}
+          aria-hidden={true}
+        />
+      </button>
+    </>
+  )
+}
