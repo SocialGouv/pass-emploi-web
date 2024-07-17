fa0105539db85c8815d449f868beaaee5ad93443
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2024-07-17 10:26:47 +0200

    fix: corrige HistoriqueAction

--- components/action/HistoriqueAction.tsx
+++ components/action/HistoriqueAction.tsx
@@ -10,7 +10,7 @@ interface HistoriqueActionProps {
 }
 
 export function HistoriqueAction({ action }: HistoriqueActionProps) {
-  const [collapsed, setCollapsed] = useState<boolean>(true)
+  const [expanded, setExpanded] = useState<boolean>(false)
 
   const lastUpdate = toShortDate(action.lastUpdate)
   const creationDate = toShortDate(action.creationDate)
@@ -18,53 +18,52 @@ export function HistoriqueAction({ action }: HistoriqueActionProps) {
   return (
     <div className='bg-primary_lighten p-6 mt-8 rounded-base shadow-base'>
       <div
-        className={`flex justify-between cursor-pointer ${
-          collapsed ? '' : 'mb-5'
+        className={`relative flex justify-between cursor-pointer ${
+          expanded ? 'mb-5' : ''
         }`}
-        onClick={() => setCollapsed(!collapsed)}
       >
         <h2 className='text-m-bold text-primary'>Historique</h2>
         <button
           type='button'
-          onClick={(e) => {
-            e.stopPropagation()
-            setCollapsed(!collapsed)
-          }}
-          aria-expanded={collapsed}
-          className='p-2 hover:bg-white hover:rounded-l'
+          onClick={() => setExpanded(!expanded)}
+          aria-controls='historique-action'
+          aria-expanded={expanded}
+          className='p-2 hover:bg-white hover:rounded-l before:absolute before:inset-0 before:z-10'
         >
           <IconComponent
-            name={collapsed ? IconName.ChevronDown : IconName.ChevronUp}
-            title={`${collapsed ? 'Voir' : 'Cacher'} l’historique`}
+            name={expanded ? IconName.ChevronUp : IconName.ChevronDown}
+            title={`${expanded ? 'Cacher' : 'Voir'} l’historique`}
             className='h-6 w-6 fill-primary'
             focusable={false}
+            aria-labelledby='label-action-historique'
           />
-          <span className='sr-only'>
-            {collapsed ? 'Voir' : 'Cacher'} l’historique
+          <span id='label-action-historique' className='sr-only'>
+            {expanded ? 'Cacher' : 'Voir'} l’historique
           </span>
         </button>
       </div>
 
-      <div
-        className={`bg-white border border-solid border-grey_100 rounded-base p-4 ${
-          collapsed ? 'hidden' : 'mb-8'
-        }`}
-      >
-        <h2 className='text-m-bold text-grey_800 mb-6 mt-2'>
-          Historique de l’action
-        </h2>
-        <dl className='grid grid-cols-[auto_1fr] grid-rows-[repeat(4,_auto)]'>
-          <InfoAction label="Créateur de l'action" isInline={true}>
-            {action.creator}
-          </InfoAction>
-          <InfoAction label='Date de création' isInline={true}>
-            {creationDate}
-          </InfoAction>
-          <InfoAction label='Date d’actualisation' isInline={true}>
-            {lastUpdate}
-          </InfoAction>
-        </dl>
-      </div>
+      {expanded && (
+        <div
+          id='historique-action'
+          className='bg-white border border-solid border-grey_100 rounded-base p-4 mb-8'
+        >
+          <h2 className='text-m-bold text-grey_800 mb-6 mt-2'>
+            Historique de l’action
+          </h2>
+          <dl className='grid grid-cols-[auto_1fr] grid-rows-[repeat(4,_auto)]'>
+            <InfoAction label="Créateur de l'action" isInline={true}>
+              {action.creator}
+            </InfoAction>
+            <InfoAction label='Date de création' isInline={true}>
+              {creationDate}
+            </InfoAction>
+            <InfoAction label='Date d’actualisation' isInline={true}>
+              {lastUpdate}
+            </InfoAction>
+          </dl>
+        </div>
+      )}
     </div>
   )
 }
