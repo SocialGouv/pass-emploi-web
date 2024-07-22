21610375fd272755c6418d06f9b967f6ed09551b
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2024-07-22 17:20:45 +0200

    fix: icone menu profil

--- components/ActualitesMenuButton.tsx
+++ components/ActualitesMenuButton.tsx
@@ -17,7 +17,7 @@ export default function ActualitesMenuButton({
 }: ActualitesMenuButtonProps) {
   const classWidget = `SGBF-open-${getWidgetId(conseiller.structure)} w-full`
   const classMenu =
-    'flex p-2 mb-6 items-center layout_base:justify-center rounded-base layout_s:justify-start layout_l:justify-start border-2 border-primary transition-all hover:cursor-pointer hover:border-white'
+    'flex p-2 mb-6 items-center justify-center rounded-base layout_l:justify-start border-2 border-primary transition-all hover:cursor-pointer hover:border-white'
 
   useLeanBeWidget(conseiller)
 
@@ -37,7 +37,7 @@ export default function ActualitesMenuButton({
         <IconComponent
           focusable={false}
           aria-hidden={true}
-          className='inline mr-0 w-4 h-4 layout_base:w-6 layout_base:h-6 layout_l:mr-2 fill-white'
+          className='inline w-6 h-6 layout_l:mr-2 fill-white'
           name={IconName.Notification}
         />
         <span className='text-md text-left sr-only layout_l:not-sr-only break-words text-white'>
