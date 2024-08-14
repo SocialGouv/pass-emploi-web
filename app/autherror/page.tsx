4895d85bb50979e268617ab402d0fba8e32f18ef
Malek Zemni <malek.zemni@octo.com>
2024-08-14 17:55:23 +0200

    feat: ameliorer ecran erreur auth
    (cherry picked from commit e37400c72c24a26449a786a327b2eadd52b1e1a9)

--- app/autherror/page.tsx
+++ app/autherror/page.tsx
@@ -1,7 +1,11 @@
+import { StructureConseiller } from '../../interfaces/conseiller'
+
 import AuthErrorPage from 'app/autherror/AuthErrorPage'
 
 type AuthErrorSearchParams = Partial<{
   reason?: string
+  typeUtilisateur: string
+  structureUtilisateur: string
 }>
 
 export default function AuthError({
@@ -10,6 +14,7 @@ export default function AuthError({
   searchParams?: AuthErrorSearchParams
 }) {
   let erreur: string
+  let lienFormulaire: string | undefined
   switch (searchParams?.reason) {
     case 'UTILISATEUR_INEXISTANT':
       erreur =
@@ -32,12 +37,44 @@ export default function AuthError({
       erreur =
         "Veuillez vous connecter en choisissant le bon accompagnement (CEJ / BRSA / AIJ). Si vous avez changé d'accompagnement, veuillez supprimer votre compte ou contacter le support."
       break
-    default:
+    default: {
       erreur =
-        'Une erreur inconnue est survenue, veuillez réessayer ou contacter le support.'
-      if (searchParams?.reason) erreur += ` (code: ${searchParams.reason})`
-      break
+        'Une erreur inconnue est survenue, veuillez essayer les solutions suivantes :\n\n- Recharger cette page\n- Activer les cookies\n- Supprimer le cache navigateur'
+
+      if (searchParams?.typeUtilisateur === 'JEUNE') {
+        erreur +=
+          '\n\nVeuillez contacter votre conseiller si le problème persiste.'
+      } else if (searchParams?.typeUtilisateur === 'CONSEILLER') {
+        lienFormulaire = ((): string | undefined => {
+          switch (searchParams.structureUtilisateur) {
+            case StructureConseiller.MILO:
+              return (
+                (process.env.NEXT_PUBLIC_FAQ_MILO_EXTERNAL_LINK as string) +
+                'assistance/'
+              )
+            case StructureConseiller.POLE_EMPLOI:
+              return (
+                (process.env.NEXT_PUBLIC_FAQ_PE_EXTERNAL_LINK as string) +
+                'formuler-une-demande/'
+              )
+            case StructureConseiller.POLE_EMPLOI_BRSA:
+            case StructureConseiller.POLE_EMPLOI_AIJ:
+              return (
+                (process.env
+                  .NEXT_PUBLIC_FAQ_PASS_EMPLOI_EXTERNAL_LINK as string) +
+                'assistance/'
+              )
+          }
+        })()
+      }
+    }
   }
 
-  return <AuthErrorPage erreur={erreur} />
+  return (
+    <AuthErrorPage
+      erreur={erreur}
+      codeErreur={searchParams?.reason}
+      lienFormulaire={lienFormulaire}
+    />
+  )
 }
