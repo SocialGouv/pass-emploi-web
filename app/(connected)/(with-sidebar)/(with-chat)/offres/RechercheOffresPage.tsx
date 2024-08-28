8aa27bafaa12dff521b9feb82e0d56279afd5109
anhara.akter <anhara.akter@octo.com>
2024-08-28 15:11:27 +0200

    fix(us-2804): fix retour

--- app/(connected)/(with-sidebar)/(with-chat)/offres/RechercheOffresPage.tsx
+++ app/(connected)/(with-sidebar)/(with-chat)/offres/RechercheOffresPage.tsx
@@ -312,7 +312,6 @@ function RechercheOffresPage() {
               title={`${collapsed ? 'Voir' : 'Cacher'} les critères`}
               className='h-6 w-6 fill-primary'
               focusable={false}
-              aria-hidden='true'
             />
             <span className='sr-only'>
               {collapsed ? 'Voir' : 'Cacher'} les critères
