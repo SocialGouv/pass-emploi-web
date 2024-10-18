6615573dba7736f225b14d40686737c6bf14fba1
Theo Gautier <theo.gautier@octo.com>
2024-10-18 11:36:58 +0200

    fix: retire plan du site login page

--- app/(connexion)/login/LayoutLoginClient.tsx
+++ app/(connexion)/login/LayoutLoginClient.tsx
@@ -69,6 +69,7 @@ export default function LayoutLoginClient({
                   : StructureConseiller.POLE_EMPLOI_BRSA,
               }}
               aDesBeneficiaires={null}
+              planDuSiteEstCache={true}
             />
           )}
         </div>
