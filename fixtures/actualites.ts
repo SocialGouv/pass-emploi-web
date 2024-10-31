312b4ad1ee097c7c28063f4d05ee0f5bda2ba7dc
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2024-10-31 15:21:04 +0100

    fix: retours revue

--- fixtures/actualites.ts
+++ fixtures/actualites.ts
@@ -1,3 +1,9 @@
-export const uneActualite = () => {
-  return "<h2 class='wp-block-heading'>Nouveauté</h2><a href='http://www.google.com'>http://www.google.com</a>"
+import { Actualites } from 'interfaces/actualites'
+
+export const uneActualite = (): Actualites => {
+  return {
+    contenu:
+      "<h2 class='wp-block-heading'>Nouveauté</h2><a href='http://www.google.com'>http://www.google.com</a>",
+    dateDerniereModification: '2024-10-30',
+  }
 }
