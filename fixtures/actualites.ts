2b91a4e0001c5058653ecfb57161d2896d706d85
Theo Gautier <theo.gautier@octo.com>
2024-11-20 16:07:52 +0100

    feat: actus par article

--- fixtures/actualites.ts
+++
@@ -1,9 +0,0 @@
-import { Actualites } from 'interfaces/actualites'
-
-export const uneActualite = (): Actualites => {
-  return {
-    contenu:
-      '<h3>Invitation à la journée présentiel du 31 octobre 2024</h3><p>Rdv demain aux nouveaux locaux de la Fabrique</p><hr />',
-    dateDerniereModification: '2024-10-30',
-  }
-}
--- tests/components/ActualitesModal.test.tsx
+++ tests/components/ActualitesModal.test.tsx
@@ -11,60 +11,102 @@ jest.mock('components/ModalContainer')
 describe('ActualitesModal', () => {
   let container: HTMLElement
 
-  const actualites = {
-    contenu:
-      '<h3>Invitation à la journée présentiel du 31 octobre 2024</h3><pre><code>Catégorie</code></pre><p>Rdv demain aux nouveaux locaux de la Fabrique</p><a href="www.google.com">Google</a><img src="pouetImg.jpg" alt="pouet"/><hr />',
-    dateDerniereModification: '2024-01-01',
-  }
-  const conseiller = unConseiller()
+  describe('quand le conseiller a des actualités', () => {
+    const article = {
+      titre: 'Invitation à la journée présentiel du 31 octobre 2024',
+      contenu: (
+        <>
+          <span>Catégorie</span>
+          <p>Rdv demain aux nouveaux locaux de la Fabrique</p>
+          <a href='www.google.com'>Google</a>
+          <img src='pouetImg.jpg' alt='pouet' />
+        </>
+      ),
+      dateDerniereModification: '2024-01-01',
+      id: 'id-article-1',
+    }
 
-  beforeEach(() => {
-    let onClose = jest.fn()
-    ;({ container } = render(
-      <ConseillerProvider conseiller={conseiller}>
-        <ActualitesProvider actualitesForTests={actualites}>
-          <ActualitesModal onClose={onClose} />
-        </ActualitesProvider>
-      </ConseillerProvider>
-    ))
-  })
+    const actualites = {
+      articles: [article],
+      dateDerniereModification: '2024-01-01',
+    }
+    const conseiller = unConseiller()
 
-  it('a11y', async () => {
-    let results: AxeResults
-
-    await act(async () => {
-      results = await axe(container)
+    beforeEach(() => {
+      let onClose = jest.fn()
+      ;({ container } = render(
+        <ConseillerProvider conseiller={conseiller}>
+          <ActualitesProvider actualitesForTests={actualites}>
+            <ActualitesModal onClose={onClose} />
+          </ActualitesProvider>
+        </ConseillerProvider>
+      ))
     })
-    expect(results!).toHaveNoViolations()
+
+    it('a11y', async () => {
+      let results: AxeResults
+      await act(async () => {
+        results = await axe(container)
+      })
+      expect(results!).toHaveNoViolations()
+    })
+
+    it('Affiche la modale', () => {
+      //Then
+      expect(
+        screen.getByRole('heading', {
+          level: 3,
+          name: 'Invitation à la journée présentiel du 31 octobre 2024',
+        })
+      )
+      expect(screen.getByText('Catégorie')).toBeInTheDocument()
+      expect(
+        screen.getByText('Rdv demain aux nouveaux locaux de la Fabrique')
+      ).toBeInTheDocument()
+      expect(
+        screen.getByRole('link', {
+          name: 'Google',
+        })
+      ).toHaveAttribute('href', 'www.google.com')
+      expect(
+        screen.getByRole('img', {
+          name: 'pouet',
+        })
+      ).toBeInTheDocument()
+    })
   })
 
-  it('Affiche la modale', () => {
-    //Then
-    expect(
-      screen.getByRole('heading', {
-        level: 3,
-        name: 'Invitation à la journée présentiel du 31 octobre 2024',
-      })
-    )
-    expect(screen.getByText('Catégorie')).toBeInTheDocument()
-    expect(
-      screen.getByText('Rdv demain aux nouveaux locaux de la Fabrique')
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole('link', {
-        name: 'Google',
-      })
-    ).toHaveAttribute('href', 'www.google.com')
-    expect(
-      screen.getByRole('img', {
-        name: 'pouet',
-      })
-    ).toBeInTheDocument()
-  })
+  describe('quand le conseiller n’a pas d’actualité', () => {
+    const actualites = {
+      articles: [],
+      dateDerniereModification: '2024-01-01',
+    }
+    const conseiller = unConseiller()
 
-  it('échape les balises pre, code et hr', () => {
-    expect(container.querySelector('hr')).not.toBeInTheDocument()
-    expect(container.querySelector('pre')).not.toBeInTheDocument()
-    expect(container.querySelector('code')).not.toBeInTheDocument()
+    beforeEach(() => {
+      let onClose = jest.fn()
+      ;({ container } = render(
+        <ConseillerProvider conseiller={conseiller}>
+          <ActualitesProvider actualitesForTests={actualites}>
+            <ActualitesModal onClose={onClose} />
+          </ActualitesProvider>
+        </ConseillerProvider>
+      ))
+    })
+
+    it('a11y', async () => {
+      let results: AxeResults
+      await act(async () => {
+        results = await axe(container)
+      })
+      expect(results!).toHaveNoViolations()
+    })
+
+    it('Affiche la modale avec un message d’erreur', () => {
+      //Then
+      expect(
+        screen.getByText('Vous n’avez pas d’actualités en cours')
+      ).toBeInTheDocument()
+    })
   })
 })
