922918ad27c1e18db7448dd2adcc0c9168fc0cb3
anhara.akter <anhara.akter@octo.com>
2024-12-05 16:57:45 +0100

    feat(us-3078): ajout de bouton trierPardernier activiter

--- components/jeune/TableauBeneficiaires.tsx
+++ components/jeune/TableauBeneficiaires.tsx
@@ -2,11 +2,16 @@ import { DateTime } from 'luxon'
 import dynamic from 'next/dynamic'
 import React, { ForwardedRef, forwardRef, useEffect, useState } from 'react'
 
+import SortIcon from '../ui/SortIcon'
+
 import EmptyState from 'components/EmptyState'
 import { IllustrationName } from 'components/ui/IllustrationComponent'
 import Pagination from 'components/ui/Table/Pagination'
 import Table from 'components/ui/Table/Table'
-import { BeneficiaireAvecInfosComplementaires } from 'interfaces/beneficiaire'
+import {
+  BaseBeneficiaire,
+  BeneficiaireAvecInfosComplementaires,
+} from 'interfaces/beneficiaire'
 import { estMilo } from 'interfaces/conseiller'
 import { useConseiller } from 'utils/conseiller/conseillerContext'
 import { toShortDate } from 'utils/date'
@@ -36,14 +41,36 @@ function TableauBeneficiaires(
 
   const DEBUT_PERIODE = DateTime.now().startOf('week')
   const FIN_PERIODE = DateTime.now().endOf('week')
+  const [triDerniereActiviter, setTriDerniereActiviter] = useState(true)
+  const [triBeneficiaires, setTriBeneficiaires] = useState(beneficiairesFiltres)
+
+  const trierParDerniereActivite = () => {
+    const nouvelleTri = !triDerniereActiviter
+    const triBeneficiaires = [...beneficiairesFiltres].sort((a, b) => {
+      if (!a.isActivated && !b.isActivated) return 0
+      if (!a.isActivated) return 1
+      if (!b.isActivated) return -1
+
+      const dateA = DateTime.fromISO(a.lastActivity!)
+      const dateB = DateTime.fromISO(b.lastActivity!)
+      return nouvelleTri
+        ? dateB.diff(dateA).milliseconds
+        : dateA.diff(dateB).milliseconds
+    })
+
+    setTriBeneficiaires(triBeneficiaires)
+    setPage(1)
+    setTriDerniereActiviter(nouvelleTri)
+  }
 
   useEffect(() => {
+    setTriBeneficiaires(beneficiairesFiltres)
     setPage(1)
   }, [beneficiairesFiltres])
 
   return (
     <>
-      {beneficiairesFiltres.length === 0 && (
+      {triBeneficiaires.length === 0 && (
         <EmptyState
           shouldFocus={true}
           illustrationName={IllustrationName.People}
@@ -52,24 +79,38 @@ function TableauBeneficiaires(
         />
       )}
 
-      {beneficiairesFiltres.length > 0 && (
+      {triBeneficiaires.length > 0 && (
         <>
           <h2 className='text-m-bold mb-2 text-center text-grey_800'>
             Semaine du {toShortDate(DEBUT_PERIODE)} au{' '}
             {toShortDate(FIN_PERIODE)}
           </h2>
 
+          <button
+            onClick={trierParDerniereActivite}
+            className='flex float-right mt-8 mb-8 text-m-regular text-right text-grey_800'
+            title={
+              triDerniereActiviter
+                ? 'Trier par dernière activité ordre anticronologique'
+                : 'Trier par dernière activité ordre cronologique'
+            }
+            type='button'
+          >
+            Trier par dernière activité
+            <SortIcon isDesc={triDerniereActiviter} />
+          </button>
+
           <Table
             ref={ref}
             caption={{
               text: 'Liste des bénéficiaires',
-              count: total === beneficiairesFiltres.length ? total : undefined,
+              count: total === triBeneficiaires.length ? total : undefined,
               visible: true,
             }}
           >
             {estMilo(conseiller) && (
               <TableauBeneficiairesMilo
-                beneficiairesFiltres={beneficiairesFiltres}
+                beneficiairesFiltres={triBeneficiaires}
                 page={page}
                 total={total}
               />
@@ -77,7 +118,7 @@ function TableauBeneficiaires(
 
             {!estMilo(conseiller) && (
               <TableauBeneficiairesPasMilo
-                beneficiairesFiltres={beneficiairesFiltres}
+                beneficiairesFiltres={triBeneficiaires}
                 page={page}
                 total={total}
               />
