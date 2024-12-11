720819b0aa8db4f582f6ebaa60054f565d00eeea
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2024-12-11 16:09:26 +0100

    build: react compiler lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'node:url'
 import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
 import prettierConfigRecommended from 'eslint-plugin-prettier/recommended'
+import reactCompiler from 'eslint-plugin-react-compiler'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -41,6 +42,14 @@ const eslintConfig = [
         'error',
         { ul: ['tablist'] },
       ],
+    },
+  },
+  {
+    plugins: {
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      'react-compiler/react-compiler': 'error',
     },
   },
   {
