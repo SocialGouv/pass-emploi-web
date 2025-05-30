8240f754f2aa92df92112edf751dbfeb07bb266a
GotchiT2 <gotchit@outlook.fr>
2025-05-30 11:12:33 +0200

    fix: ren intensif wording

--- interfaces/structure.ts
+++ interfaces/structure.ts
@@ -90,7 +90,7 @@ export function labelStructure(structure: StructureFTConnect): string {
     case structureAij:
       return 'AIJ'
     case structureAccompagnementIntensif:
-      return 'REN-Intensif'
+      return 'REN-Intensif / FTT-FTX'
     case structureAccompagnementGlobal:
       return 'Accompagnement global'
     case structureEquipEmploiRecrut:
